55ee2d60089cb70fd55c33d9cc8d6d5d926ea105
ldh <diliu@deloitte.com.cn>
2021-02-24 10:04:57 +0800

    update topLayout components

---
+++ src/layout/components/topLayout/input.vue
@@ -0,0 +1,63 @@
+<template>
+  <iInput class="search" :placeholder="$t('search')" :value="value" @input="handleInput">
+    <icon class="icon" slot="prefix" name="iconsousudingbu" />
+  </iInput>
+</template>
+
+<script>
+import { iInput, icon } from '@/components';
+
+export default {
+  components: {
+    iInput,
+    icon
+  },
+  props: {
+    value: {
+      type: String || Number,
+      default: ''
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit('input', e)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search {
+  width: 667px;
+  height: 50px;
+  line-height: 50px;
+  background: #F8F8FA;
+  border-radius: 10px;
+
+  .icon {
+    height: 100%;
+    margin-right: 20px;
+    font-size: 26px;
+  }
+
+  ::v-deep .el-input__prefix {
+    width: 76px;
+    text-align: right;
+  }
+
+  ::v-deep .el-input__inner {
+    height: 50px;
+    padding: 0 40px 0 76px;
+    background: transparent;
+    box-shadow: 0 0 0 0;
+
+    &::placeholder {
+      color: $color-header-black;
+      opacity: 0.3;
+    }
+  }
+}
+</style>
