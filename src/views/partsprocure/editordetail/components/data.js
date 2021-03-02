5b1b40ff6bfb2ba266d680885c1429d4da8107b1
周玉松 <yuszhou@deloitte.com.cn>
2021-03-02 16:02:19 +0800

    update detail pages

--- src/views/partsprocure/editordetail/components/data.js
+++ src/views/partsprocure/editordetail/components/data.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-25 10:10:50
- * @LastEditTime: 2021-02-25 10:10:51
+ * @LastEditTime: 2021-03-02 15:47:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \rise\src\views\partsprocure\editordetail\components\data.js
