6267afeae52de5dbb95648170b92a6debcaf3bcf
周玉松 <yuszhou@deloitte.com.cn>
2021-03-08 09:45:42 +0800

    update code

--- src/components/iInput/index.vue
+++ src/components/iInput/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: yuszhou
  * @Date: 2020-07-30 12:23:08
- * @LastEditTime: 2021-02-22 17:45:36
+ * @LastEditTime: 2021-03-08 09:44:27
  * @LastEditors: Please set LastEditors
  * @Description: 基于element的Input组件，支持element全属性
  * @FilePath: \Front\src\components\iInput\index.vue
@@ -17,6 +17,11 @@ export default{
     height: $input-height;
     .el-input__inner{
       @include input_inner;
+    }
+  }
+  .el-textarea{
+    .el-textarea__inner{
+      @include textarea_inner;
     }
   }
 </style>
