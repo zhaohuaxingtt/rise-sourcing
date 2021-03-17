003f7be49eade2f84773ab77906d85e0726c51f1
周玉松 <yuszhou@deloitte.com.cn>
2021-03-17 16:38:56 +0800

    'init'

--- src/api/partsign/home/index.js
+++ src/api/partsign/home/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yuszhou
  * @Date: 2021-02-24 09:55:16
- * @LastEditTime: 2021-03-17 12:51:19
+ * @LastEditTime: 2021-03-17 16:35:27
  * @LastEditors: Please set LastEditors
  * @Description: 零件号签收列表
  * @FilePath: \rise\src\api\partsign\home\index.js
@@ -12,9 +12,21 @@ const requst = axios(process.env.VUE_APP_PARTS)
 //获取信息单列表接口。
 export function getTabelData(parmars){
   return requst({
-    url:'/tpInfo/backManage/getInfo',
+    url:'/tp-records',
     method:'get',
-    data:parmars
+    data:{
+      tpRecordsSenario:parmars
+    }
+  })
+}
+//获取列表上方Group信息
+export function getPageGroup(userId){
+  return requst({
+    url:'/tp-records',
+    method:'get',
+    data:{
+      groupStatSenario:userId
+    }
   })
 }
 export function getInquiryBuyerList(parmars){
