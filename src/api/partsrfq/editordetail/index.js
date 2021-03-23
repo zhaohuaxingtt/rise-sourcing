ca1b2dd8f68dbdd0a8744e2e9aff221a2b9477eb
moxuan <xmo@deloitte.com.cn>
2021-03-23 20:05:15 +0800

    rfq update

--- src/api/partsrfq/editordetail/index.js
+++ src/api/partsrfq/editordetail/index.js
@@ -1,70 +1,88 @@
 import axios from '@/utils/axios'
+
 const requst = axios(process.env.VUE_APP_RFQ)
 
 /*供应商评分*/
 export function getAllSupplier(parmars) {
-  return requst({
-    url: '/api/getAllSupplier',
-    method: 'POST',
-    data: parmars
-  })
+    return requst({
+        url: '/api/getAllSupplier',
+        method: 'POST',
+        data: parmars
+    })
 }
 
 export function getAllAnnex(parmars) {
-  return requst({
-    url: '/api/getAllAnnex',
-    method: 'POST',
-    data: parmars
-  })
+    return requst({
+        url: '/api/getAllAnnex',
+        method: 'POST',
+        data: parmars
+    })
 }
 
 export function deleteAnnex(parmars) {
-  return requst({
-    url: '/api/deleteAnnex',
-    method: 'POST',
-    data: parmars
-  })
+    return requst({
+        url: '/api/deleteAnnex',
+        method: 'POST',
+        data: parmars
+    })
 }
+
+export function setTpbMemo(parmars) {
+    return requst({
+        url: '/api/setTpbMemo',
+        method: 'POST',
+        data: parmars
+    })
+}
+
+export function uploadRfqAnnex(parmars) {
+    return requst({
+        url: '/api/uploadRfqAnnex',
+        method: 'POST',
+        data: parmars
+    })
+}
+
 
 /*模具预算申请*/
 export function getModelBudgetList(parmars) {
-  return requst({
-    url: '/api/getModelBudgetList',
-    method: 'POST',
-    data: parmars
-  })
+    return requst({
+        url: '/api/getModelBudgetList',
+        method: 'POST',
+        data: parmars
+    })
 }
 
 export function submitMoldBudget(parmars) {
-  return requst({
-    url: '/api/submitMoldBudget',
-    method: 'POST',
-    data: parmars
-  })
+    return requst({
+        url: '/api/submitMoldBudget',
+        method: 'POST',
+        data: parmars
+    })
 }
 
 export function cancelMoldBudget(parmars) {
-  return requst({
-    url: '/api/cancelMoldBudget',
-    method: 'POST',
-    data: parmars
-  })
+    return requst({
+        url: '/api/cancelMoldBudget',
+        method: 'POST',
+        data: parmars
+    })
 }
 
 /*技术交流会*/
 export function getAllRfqParts(parmars) {
-  return requst({
-    url: '/api/getAllRfqParts',
-    method: 'POST',
-    data: parmars
-  })
+    return requst({
+        url: '/api/getAllRfqParts',
+        method: 'POST',
+        data: parmars
+    })
 }
 
 
 export function getAllRfqSupplier(parmars) {
-  return requst({
-    url: '/api/getAllRfqSupplier',
-    method: 'POST',
-    data: parmars
-  })
+    return requst({
+        url: '/api/getAllRfqSupplier',
+        method: 'POST',
+        data: parmars
+    })
 }
