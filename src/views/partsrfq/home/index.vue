f0e3b7e2dcc68d89b21d2e32efffd33eca3613a9
moxuan <xmo@deloitte.com.cn>
2021-03-23 19:38:05 +0800

    rfq  update

--- src/views/partsrfq/home/index.vue
+++ src/views/partsrfq/home/index.vue
@@ -124,7 +124,7 @@ export default {
       tableTitle: tableTitle,
       tableLoading: false,
       selectTableData: [],
-      diologAssignmentOfScroingTasks: true,
+      diologAssignmentOfScroingTasks: false,
       form: {
         searchConditions: '',
         carType: '',
