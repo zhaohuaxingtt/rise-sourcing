19a5dcaf769b10a19a837ada9abaedd0b6f3bad9
周玉松 <yuszhou@deloitte.com.cn>
2021-03-24 14:35:44 +0800

    git pull

--- vue.config.js
+++ vue.config.js
@@ -158,8 +158,8 @@ module.exports = {
 				}
 			},
 			'/usercenterApi': { // usercenter.../唐开雄
-				//target: 'http://10.122.18.166:8015',
-				target:'http://10.160.137.165:8015',
+				target: 'http://10.122.18.166:8015',
+				// target:'http://10.160.137.165:8015',
 				changeOrigin: true,
 				pathRewrite: {
 					"^/usercenterApi": ""
