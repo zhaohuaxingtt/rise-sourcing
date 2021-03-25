276f8fe840ab913e99317e4107658617292e4987
舒杰 <jieshu@deloitte.com.cn>
2021-03-25 14:01:46 +0800

    update 批量维护零件

--- src/views/partsprocure/batchmiantain/components/data.js
+++ src/views/partsprocure/batchmiantain/components/data.js
@@ -3,10 +3,10 @@ export const tableTitle = [
   { props: 'partNameZh', name: '零件名（中）' },
   { props: 'partNameDe', name: '零件名（德）' },
   { props: 'cartypeProjectZh', name: '车型项目' },
-  { props: 'd', name: '开始年份' },
-  { props: 'e', name: '+0' },
-  { props: 'f', name: '+1' },
-  { props: 'g', name: '+2' },
-  { props: 'h', name: '+3' },
-  { props: 'i', name: '+4' }
+  { props: 'startYear', name: '开始年份' },
+  { props: 'year0', name: '+0' },
+  { props: 'year1', name: '+1' },
+  { props: 'year2', name: '+2' },
+  { props: 'year3', name: '+3' },
+  { props: 'year4', name: '+4' }
 ]
--- src/views/partsprocure/batchmiantain/components/outputPlan.vue
+++ src/views/partsprocure/batchmiantain/components/outputPlan.vue
@@ -1,66 +1,89 @@
 <template>
-  <iCard class="outputPlan" title="零件产量计划" tabCard>
-    <div class="body">
-      <tablelist
-        class="table"
-        index
-        :tableData="tableListData" 
-        :tableTitle="tableTitle" 
-        :tableLoading="loading" />
-      <iPagination
-        class="pagination margin-top30"
-        @size-change="handleSizeChange($event, getUsage)"
-        @current-change="handleCurrentChange($event, getUsage)"
-        background
-        :current-page="page.size"
-        :page-sizes="page.pageSizes"
-        :page-size="page.pageSize"
-        :layout="page.layout"
-        :total="page.totalCount" />
-    </div>
-  </iCard>
+	<iCard class="outputPlan" title="零件产量计划" tabCard>
+		<div class="body">
+			<tablelist class="table" index :tableData="tableListData" :tableTitle="tableTitle"
+				:tableLoading="loading" @handleSelectionChange="handleSelectionChange" activeItems="partNum" @openPage="openPage"/>
+			<iPagination class="pagination margin-top30" @size-change="handleSizeChange($event, getUsage)"
+				@current-change="handleCurrentChange($event, getUsage)" background :current-page="page.size"
+				:page-sizes="page.pageSizes" :page-size="page.pageSize" :layout="page.layout"
+				:total="page.totalCount" />
+		</div>
+	</iCard>
 </template>
 
 <script>
-import { iCard, iPagination } from '@/components'
-import tablelist from '@/views/partsign/home/components/tableList'
-import { pageMixins } from '@/utils/pageMixins'
-import { tableTitle } from './data'
-import {getAllTable} from "@/api/partsprocure/home";
-export default {
-  components: { iCard, tablelist, iPagination },
-  mixins: [ pageMixins ],
-  data() {
-    return {
-      loading: false,
-      tableTitle,
-      tableListData: [],
-	  purchaseProjectIds:[]
-    }
-  },
-  created() {
-  	this.purchaseProjectIds = this.$route.query.ids;
-	this.getData()
-  },
-  methods: {
-	  // 获取批量数据
-	  getData() {
-	  	getAllTable(this.getIds(this.purchaseProjectIds)).then((res) => {
-	  			this.tableListData = res.data.partOutPutPlanBatchs;
-	  		})
-	  },
-	  // 组装请求ids\
-	  getIds(arr){
-	  	let url = ''
-	  	for (let i = 0; i < arr.length; i++) {
-	  		url += 'partOutputPlanByBatchFacadeDTO.purchaseProjectIds='+arr[i]+(i == arr.length-1?'':"&")
-	  	}
-	  	return url
-	  }
-  }
-}
+	import {
+		iCard,
+		iPagination
+	} from '@/components'
+	import tablelist from '@/views/partsign/home/components/tableList'
+	import {
+		pageMixins
+	} from '@/utils/pageMixins'
+	import {
+		tableTitle
+	} from './data'
+	import {
+		getAllTable
+	} from "@/api/partsprocure/home";
+	export default {
+		components: {
+			iCard,
+			tablelist,
+			iPagination
+		},
+		mixins: [pageMixins],
+		data() {
+			return {
+				loading: false,
+				tableTitle,
+				tableListData: [],
+				purchaseProjectIds: []
+			}
+		},
+		created() {
+			this.purchaseProjectIds = this.$route.query.ids;
+			this.getData()
+		},
+		methods: {
+			// 获取批量数据
+			getData() {
+				getAllTable(this.getIds(this.purchaseProjectIds)).then((res) => {
+					if (res.data.partOutPutPlanBatchs) {
+						let  arr=res.data.partOutPutPlanBatchs
+						arr.forEach(res => {
+							res.outputPlanList.forEach((val, i) => {
+								res. ['year' + i] = val.outPut
+							})
+							res.startYear = res.outputPlanList[0].year
+						})
+						this.tableListData = arr;
+					}
+				})
+			},
+			// 组装请求ids\
+			getIds(arr) {
+				let url = ''
+				for (let i = 0; i < arr.length; i++) {
+					url += 'partOutputPlanByBatchFacadeDTO.purchaseProjectIds=' + arr[i] + (i == arr.length - 1 ? '' : "&")
+				}
+				return url
+			},
+			handleSelectionChange(e){
+				this.$emit('handleSelectionChange',e)
+			},
+			openPage(item){
+				this.$router.push({
+					path:"/partsprocure/outputplan",
+					query:{
+						purchaseProjectIds:item.purchaseProjectIds
+					}
+				})
+			}
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
-.outputPlan {}
+	.outputPlan {}
 </style>
