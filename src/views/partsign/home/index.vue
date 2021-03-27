c96181441b94fb707bda5676895056ecf9a5a2ae
moxuan <xmo@deloitte.com.cn>
2021-03-27 21:36:42 +0800

    update

--- src/views/partsign/home/index.vue
+++ src/views/partsign/home/index.vue
@@ -9,7 +9,7 @@
 <template>
   <iPage class="partsignHome">
     <el-tabs v-model="tab" class="tab">
-      <el-tab-pane :label="$t('XunYuanZhiXing')" name="source">
+      <el-tab-pane :label="$t('partsignLanguage.XunYuanZhiXing')" name="source">
         <div>
           <div class="margin-bottom33">
             <iNavMvp @change="change" right routerPage></iNavMvp>
