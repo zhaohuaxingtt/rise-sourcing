b37e96b3e9b89e76eccdecc53e58d610a0c80f73
moxuan <xmo@deloitte.com.cn>
2021-03-29 09:35:49 +0800

    update

--- src/views/partsrfq/editordetail/components/rfqDetailInfo/components/targetPrice/components/data.js
+++ src/views/partsrfq/editordetail/components/rfqDetailInfo/components/targetPrice/components/data.js
@@ -15,5 +15,5 @@ export const tableTitle = [
     {props:'skdBPrice',name:'SKD_B'},
     {props:'ckdDuty',name:'CKD Duty%'},
     {props:'ckdExwork',name:'CKD EX-Work'},
-    {props:'skdLanede',name:'CKDLANDED'}
+    {props:'ckdLanded',name:'CKDLANDED'}
 ]
