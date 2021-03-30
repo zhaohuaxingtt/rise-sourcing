27ae38fd2e7a145f56fc6614612187598fd500b5
ldh <diliu@deloitte.com.cn>
2021-03-30 14:49:06 +0800

    fix bug

--- src/views/partsrfq/editordetail/components/rfqPending/components/BDL/data.js
+++ src/views/partsrfq/editordetail/components/rfqPending/components/BDL/data.js
@@ -8,7 +8,7 @@ export const tableTitle = [
     {props:'sapNum',name:'供应商SAP号'},
     {props:'supplierNameZh',name:'供应商名称'},
     {props:'pupplierProductionPlace',name:'供应商生产场地'},
-    {props:'isMbdl',name:'MBDL'},
+    {props:'bdlType',name:'MBDL'},
     {props:'isCheckCbd',name:'是否验证CBD'},
     {props:'supplierQualityGrade',name:'质量'},
     {props:'supplierCostGrade',name:'成本'},
