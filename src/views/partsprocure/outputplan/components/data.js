5d141f1597d20be8b70434474261a6b01e133733
EXT15805 <ext-wangwei4@csvw.com>
2021-03-31 15:38:07 +0800

    数据处理11

--- src/views/partsprocure/outputplan/components/data.js
+++ src/views/partsprocure/outputplan/components/data.js
@@ -1,24 +1,24 @@
 export const outputPlanTableTitle = [
   { props: 'pc', name: '年份', key: 'LK_NIANFEN' },
-  { props: 'totalOutput', name: 'SUM' },
+  { props: 'totalOutput', name: 'SUM', key: 'LK_SUM' },
   { props: 'versionNum', name: '版本号', key: 'LK_BANBENHAO' }
 ]
 
 export const outputRecordTableTitle = [
-  { props: 'totalOutput', name: 'SUM' },
+  { props: 'totalOutput', name: 'SUM', key: 'LK_SUM' },
   { props: 'versionNum', name: '版本号', key: 'LK_BANBENHAO' },
   { props: 'updateReason', name: '更新原因', key: 'LK_GENGXINYUANYIN' }
 ]
 
 export const volumeTableTitle = [
-  { props: 'partNum', name: '零件号' },
-  { props: 'partNameCn', name: '零件名（中）' },
-  { props: 'partNameDe', name: '零件名（德）' },
-  { props: 'd', name: '配置编码' },
-  { props: 'e', name: '发动机配置' },
-  { props: 'f', name: '变速箱配置' },
-  { props: 'g', name: '等级配置' },
-  { props: 'i', name: '其他配置' },
-  { props: 'j', name: '配置比例' },
-  { props: 'perCarDosage', name: '用量' }
+  { props: 'partNum', name: '零件号', key: 'LK_LINGJIANHAO' },
+  { props: 'partNameCn', name: '零件名（中）', key: 'LK_LINGJIANMINGCHENGZHONG' },
+  { props: 'partNameDe', name: '零件名（德）', key: 'LK_LINGJIANMINGDE' },
+  { props: 'd', name: '配置编码', key: 'LK_PEIZHIBIANMA' },
+  { props: 'e', name: '发动机配置', key: 'LK_FADONGJIPEIZHI'  },
+  { props: 'f', name: '变速箱配置', key: 'LK_BIANSUXIANGPEIZHI' },
+  { props: 'g', name: '等级配置', key: 'LK_DENGJIPEIZHI' },
+  { props: 'i', name: '其他配置', key: 'LK_QITAPEIZHI' },
+  { props: 'j', name: '配置比例', key: 'LK_PEIZHIBILI'  },
+  { props: 'perCarDosage', name: '用量', key: 'LK_YONGLIANG' }
 ]
