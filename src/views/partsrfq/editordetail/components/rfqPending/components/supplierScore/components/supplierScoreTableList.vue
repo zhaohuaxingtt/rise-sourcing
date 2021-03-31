da729c82838a7ad2d9ded25b8ab51b81715a172e
moxuan <xmo@deloitte.com.cn>
2021-03-31 23:08:48 +0800

    fix

--- src/views/partsrfq/editordetail/components/rfqPending/components/supplierScore/components/supplierScoreTableList.vue
+++ src/views/partsrfq/editordetail/components/rfqPending/components/supplierScore/components/supplierScoreTableList.vue
@@ -5,30 +5,35 @@
  * @Description: RFQ-table组件。
 -->
 <template>
-  <el-table :height="height" :data='tableData' :empty-text="$t('LK_ZANWUSHUJU')" v-loading='tableLoading' @selection-change="handleSelectionChange">
+  <el-table :height="height" :data='tableData' :empty-text="$t('LK_ZANWUSHUJU')" v-loading='tableLoading'
+            @selection-change="handleSelectionChange">
     <el-table-column v-if="selection" type='selection' width="50" align='center'></el-table-column>
-<!--    <el-table-column v-if='index' type='index' width='50' align='center' label='#'></el-table-column>-->
+    <!--    <el-table-column v-if='index' type='index' width='50' align='center' label='#'></el-table-column>-->
     <template v-for="(items,index) in tableTitle">
       <template v-if="items.list && isArray(items.list)">
-        <el-table-column :label="$t(items.key)" :key="index" align="center">
+        <el-table-column :label="items.key ? $t(items.key) : items.name" :key="index" align="center">
           <template v-for="(items2, index2) in items.list">
-            <el-table-column :key="index2" align='center' v-if="items2.props === multiHeaderProps" :prop="items2.props" :label="items2.name">
+            <el-table-column :key="index2" align='center' v-if="items2.props === multiHeaderProps" :prop="items2.props"
+                             :label="items2.name">
               <template slot-scope="scope">
-                <span class="openLinkText cursor" @click="openMultiHeaderPropsPage(scope.row)">{{multiHeaderPropsText}}</span>
+                <span class="openLinkText cursor"
+                      @click="openMultiHeaderPropsPage(scope.row)">{{ multiHeaderPropsText }}</span>
               </template>
             </el-table-column>
-            <el-table-column :key="index2" align='center' v-else :label="items2.name" :prop="items2.props"></el-table-column>
+            <el-table-column :key="index2" align='center' v-else :label="items2.name"
+                             :prop="items2.props"></el-table-column>
           </template>
         </el-table-column>
       </template>
       <template v-else>
         <el-table-column :key="index" align='center' v-if="items.props === actionProps" :prop="items.props"
-                         :label="$t(items.key)">
+                         :label="items.key ? $t(items.key) : items.name">
           <template slot-scope="scope">
             <span class="openLinkText cursor" @click="openActionPropsPage(scope.row)">查看</span>
           </template>
         </el-table-column>
-        <el-table-column :key="index" align='center' v-else :label="$t(items.key)" :prop="items.props"></el-table-column>
+        <el-table-column :key="index" align='center' v-else :label="$t(items.key)"
+                         :prop="items.props"></el-table-column>
       </template>
     </template>
   </el-table>
