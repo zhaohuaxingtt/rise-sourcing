872e25facd907edc3b324c7312f981e84aa74de7
周玉松 <yuszhou@deloitte.com.cn>
2021-04-01 22:23:07 +0800

    语言切换

--- public/locale/i18n.js
+++ public/locale/i18n.js
@@ -1,4 +1,4 @@
-;(function (global, factory) {
+(function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
       typeof define === 'function' && define.amd ? define(factory) :
           (global = global || self, global.i18n = factory())
@@ -9,7 +9,14 @@
     lang = window.navigator.language ? window.navigator.language.replace(/^(.*)-(.*)$/, '$1'): 'zh'
     localStorage.setItem('lang', lang)
   }
-
+  //默认语言设置ELEMENT
+  if(lang == "en"){
+    // eslint-disable-next-line no-undef
+    ELEMENT.locale(ELEMENT.lang.en)
+  }else{
+    // eslint-disable-next-line no-undef
+    ELEMENT.locale(ELEMENT.lang.zhCN)
+  }
   return new window.VueI18n({
     locale: lang || 'zh',
     fallbackLocale: 'en',
--- src/utils/mydirect.js
+++ src/utils/mydirect.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yuszhou
  * @Date: 2021-02-19 14:29:09
- * @LastEditTime: 2021-03-28 20:55:27
+ * @LastEditTime: 2021-04-01 22:12:44
  * @LastEditors: Please set LastEditors
  * @Description: 自定义指令文件。
  * @FilePath: \rise\src\utils\mydirect.js
@@ -16,5 +16,13 @@ Vue.directive('permission', {
       el.parentNode.removeChild(el)
      }
   }
+});
+
+//切换I8n动态更新element值
+// eslint-disable-next-line no-undef
+Vue.directive('update', {
+  bind:function(el,binding,vnode){
+    vnode.key = parseInt(Math.random()*1000000000)
+  },
 });
 
