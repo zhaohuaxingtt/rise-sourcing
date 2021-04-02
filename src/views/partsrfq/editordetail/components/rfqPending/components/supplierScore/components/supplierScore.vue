add8b8f40e2b79cdea221eecaab15e5c297f7730
moxuan <xmo@deloitte.com.cn>
2021-04-02 16:15:48 +0800

    fix

--- src/views/partsrfq/editordetail/components/rfqPending/components/supplierScore/components/supplierScore.vue
+++ src/views/partsrfq/editordetail/components/rfqPending/components/supplierScore/components/supplierScore.vue
@@ -77,8 +77,7 @@ export default {
         try {
           const req = {
             rfqId: id,
-            userId: store.state.permission.userInfo.id,
-            isFake: 1
+            userId: store.state.permission.userInfo.id
           }
           const res = await getAllSupplier(req)
           this.tableListData = res.records;
