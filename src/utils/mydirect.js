8fd58642c013ecbd5b9e2cabb631c04deb6dccdd
周玉松 <yuszhou@deloitte.com.cn>
2021-04-12 21:44:09 +0800

    cache code

--- src/utils/mydirect.js
+++ src/utils/mydirect.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yuszhou
  * @Date: 2021-02-19 14:29:09
- * @LastEditTime: 2021-04-12 16:35:21
+ * @LastEditTime: 2021-04-12 20:57:25
  * @LastEditors: Please set LastEditors
  * @Description: 自定义指令文件。
  * @FilePath: \rise\src\utils\mydirect.js
@@ -12,21 +12,17 @@ import store from '../store'
 // eslint-disable-next-line no-undef
 Vue.directive('permission', {
 	inserted: function(el, binding) {
-		if (!store.state.permission.whiteBtnList[binding.expression]) {
 			if (binding.modifiers.disabled) {
-				el.classList.add("is-disabled")
-				// let textarea = el.getElementsByTagName("textarea")
-				// let input = el.getElementsByTagName("input")
-				// let arr = [...textarea,...input]
-				// arr.forEach(res => {
-				// 	res.setAttribute('disabled','disabled')
-				// })
+				if (store.state.permission.whiteBtnList[binding.expression]) {
+					el.classList.add("is-disabled")
+				}
 			} else {
-				el.parentNode.removeChild(el)
+				if (!store.state.permission.whiteBtnList[binding.expression]) {
+					el.parentNode.removeChild(el)
+				}
 			}
 		}
-	}
-});
+})
 //切换I8n动态更新element值
 // eslint-disable-next-line no-undef
 Vue.directive('update', {
--- src/views/partsign/home/index.vue
+++ src/views/partsign/home/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: yuszhou
  * @Date: 2021-02-24 09:17:57
- * @LastEditTime: 2021-04-03 12:21:51
+ * @LastEditTime: 2021-04-12 21:43:57
  * @LastEditors: Please set LastEditors
  * @Description: 零件签收列表界面.
  * @FilePath: \rise\src\views\partsign\index.vue
@@ -311,7 +311,7 @@ export default {
           csFReceiveMemo: this.backmark,
         },
       }).then((res) => {
-        if (res.data) {
+        if (res.code == 200) {
           iMessage.success(this.$t("LK_CAOZUOCHENGGONG"));
           this.getTableList();
         } else {
