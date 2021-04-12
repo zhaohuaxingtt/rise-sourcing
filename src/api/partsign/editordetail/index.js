69ef77b40d853696617093a0f0f2b4f4c1221287
周玉松 <yuszhou@deloitte.com.cn>
2021-04-12 22:50:49 +0800

    cache code

--- src/api/partsign/editordetail/index.js
+++ src/api/partsign/editordetail/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-24 10:19:28
- * @LastEditTime: 2021-04-01 10:43:14
+ * @LastEditTime: 2021-04-12 22:37:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \rise\src\api\partsign\editordetail\index.js
