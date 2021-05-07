83e731a85208c0e21d0844dc480575fa52ee5990
EXT15805 <Csvw123456>
2021-05-07 12:10:50 +0800

    1.地址配置增加tooling

--- src/api/priceorder/stocksheet/index.js
+++ src/api/priceorder/stocksheet/index.js
@@ -1,13 +1,13 @@
 import axios from '@/utils/axios'
 
-const requst = axios(process.env.VUE_APP_WS2)
+const CARTYPE = axios(process.env.VUE_APP_CARTYPE)
 
 /*零件清单*/
 
 //获取车型列表
 export function findCartypePro(parmars) {
-    return requst({
-        url: '/web/cartype/findCartypePro',
+    return CARTYPE({
+        url: '/findCartypePro',
         method: 'GET',
         params: parmars
     })
--- src/api/priceorder/stocksheet/investmentList.js
+++ src/api/priceorder/stocksheet/investmentList.js
@@ -1,82 +1,45 @@
 import axios from '@/utils/axios'
 
-const requst = axios(process.env.VUE_APP_WS2)
-//获取版本号下拉列表——按照版本创建时间降序
-export function getInvestmentVerisionList(parmars) {
-    return requst({
-        url: '/web/investmentVerision/getInvestmentVerisionList',
-        method: 'GET',
-        params: parmars
-    })
-}
+const INVESTMENT = axios(process.env.VUE_APP_INVESTMENT)
+const INVESTMENTVERSION = axios(process.env.VUE_APP_INVESTMENTVERSION)
+const BUILDINVESTMENT = axios(process.env.VUE_APP_BUILDINVESTMENT)
+const INVESTMENTORDER = axios(process.env.VUE_APP_INVESTMENTORDER)
 //获取专业科室下拉列表
 export function getDepartmentsList(parmars) {
-    return requst({
-        url: '/web/investment/getDepartmentsList',
+    return INVESTMENT({
+        url: '/getDepartmentsList',
         method: 'GET',
         params: parmars
     })
 }
 //获取专业科室下拉列表
 export function proDeptPullDown(parmars) {
-    return requst({
-        url: '/web/investment/proDeptPullDown',
+    return INVESTMENT({
+        url: '/proDeptPullDown',
         method: 'POST',
         params: parmars
-    })
-}
-//投资清单-添加行-模具属性下拉
-export function getModelProtitesPullDown(parmars) {
-    return requst({
-        url: '/web/buildInvestment/getModelProtitesPullDown',
-        method: 'GET',
-        params: parmars
-    })
-}
-//预算-自定义车型关联主数据车型
-export function relationMainCarType(parmars) {
-    return requst({
-        url: '/web/buildInvestment/relationMainCarType',
-        method: 'GET',
-        params: parmars
-    })
-}
-//生成投资清单条件查询
-export function findInvestmentBuild(parmars) {
-    return requst({
-        url: '/web/buildInvestment/findInvestmentBuild',
-        method: 'POST',
-        data: parmars
-    })
-}
-//获取投资清单数据分析数据
-export function getInvestmentData(parmars) {
-    return requst({
-        url: '/web/InvestmentOrder/getInvestmentData',
-        method: 'POST',
-        data: parmars
     })
 }
 //模具投资清单条件查询
 export function findInvestmentList(parmars) {
-    return requst({
-        url: '/web/investment/findInvestmentList',
+    return INVESTMENT({
+        url: '/findInvestmentList',
         method: 'POST',
         data: parmars
     })
 }
 //模具投资清单-添加行
 export function investmentSave(parmars) {
-    return requst({
-        url: '/web/investment/save',
+    return INVESTMENT({
+        url: '/save',
         method: 'POST',
         data: parmars
     })
 }
 //模具投资清单-刪除投资清单
 export function investmentDelete(data, parmars) {
-    return requst({
-        url: '/web/investment/delete',
+    return INVESTMENT({
+        url: '/delete',
         method: 'POST',
         data: data,
         params: parmars,
@@ -84,26 +47,67 @@ export function investmentDelete(data, parmars) {
 }
 //模具投资清单-保存编辑内容
 export function investmentUpdate(parmars) {
-    return requst({
-        url: '/web/investment/update',
+    return INVESTMENT({
+        url: '/update',
         method: 'POST',
         data: parmars
     })
 }
 //模具投资清单-保存为新版本
 export function saveNewVersion(parmars) {
-    return requst({
-        url: '/web/investment/saveNewVersion',
+    return INVESTMENT({
+        url: '/saveNewVersion',
         method: 'POST',
         data: parmars
     })
 }
 //获取linie下拉
 export function liniePullDownByDept(parmars) {
-    return requst({
-        url: '/web/investment/liniePullDownByDept',
+    return INVESTMENT({
+        url: '/liniePullDownByDept',
         method: 'POST',
         data: parmars
     })
 }
+//获取版本号下拉列表——按照版本创建时间降序
+export function getInvestmentVerisionList(parmars) {
+    return INVESTMENTVERSION({
+        url: '/getInvestmentVerisionList',
+        method: 'GET',
+        params: parmars
+    })
+}
+//投资清单-添加行-模具属性下拉
+export function getModelProtitesPullDown(parmars) {
+    return BUILDINVESTMENT({
+        url: '/getModelProtitesPullDown',
+        method: 'GET',
+        params: parmars
+    })
+}
+//预算-自定义车型关联主数据车型
+export function relationMainCarType(parmars) {
+    return BUILDINVESTMENT({
+        url: '/relationMainCarType',
+        method: 'GET',
+        params: parmars
+    })
+}
+//生成投资清单条件查询
+export function findInvestmentBuild(parmars) {
+    return BUILDINVESTMENT({
+        url: '/findInvestmentBuild',
+        method: 'POST',
+        data: parmars
+    })
+}
+//获取投资清单数据分析数据
+export function getInvestmentData(parmars) {
+    return INVESTMENTORDER({
+        url: '/getInvestmentData',
+        method: 'POST',
+        data: parmars
+    })
+}
+
 
