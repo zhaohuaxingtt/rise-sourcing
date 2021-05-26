ea4f2aa485233ed08e33a09deec79e11d1133218
wentliao <wentliao@deloitte.com.cn>
2021-05-26 19:08:16 +0800

    提交定点申请预览

--- src/layout/nomination/layout.vue
+++ src/layout/nomination/layout.vue
@@ -1,11 +1,16 @@
+<!--
+ * @Author: wentliao
+ * @Date: 2021-05-21 16:44:09
+ * @Description: 
+-->
 <template>
 <iPage>
   <div class="nomination-wraper">
     <div class="nomination-layout">
       <!-- 进度条,基本信息 -->
-      <designateStep />
+      <designateStep v-if="isPreview=='0'"/>
       <!-- 三级导航栏 -->
-      <decisionDataHeader v-if="!$route.meta.hideTabLV3" />
+      <decisionDataHeader :isPreview="isPreview" v-if="!$route.meta.hideTabLV3" />
     </div>
     <div class="nomination-content">
       <router-view></router-view>
@@ -25,6 +30,22 @@ export default {
     designateStep,
     decisionDataHeader,
     iPage
-  }
+  },
+  data(){
+    return{
+      isPreview:'0'
+    }
+  },
+  created(){
+    const {query} = this.$route;
+    const {isPreview = '0'} = query;
+    this.isPreview = isPreview;
+  },
+  watch:{$route(to,from){
+    console.log(to,from)
+    const {query={}} = to;
+    const {isPreview = '0'} = query;
+    this.isPreview = isPreview;
+  }}
 }
 </script>
