305052489d751b00d7831bfb79fe2216a76c922c
ldh <diliu@deloitte.com.cn>
2021-05-30 00:31:06 +0800

    update code

--- src/views/partsrfq/editordetail/components/rfqDetailTpzs/components/quotationScoringHz/index.vue
+++ src/views/partsrfq/editordetail/components/rfqDetailTpzs/components/quotationScoringHz/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: yuszhou
  * @Date: 2021-05-28 14:32:04
- * @LastEditTime: 2021-05-28 15:52:41
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-05-30 00:29:03
+ * @LastEditors: ldh
  * @Description: 报价分析汇总表格
  * @FilePath: \front-web\src\views\partsrfq\editordetail\components\rfqDetailTpzs\components\quotationScoringHz\index.vue
 -->
@@ -25,6 +25,7 @@
         </div>            
       </div>
       <div class="btnSearch">
+        <iButton @click="quote">是否引用询价轮次报价</iButton>
         <iButton>组合</iButton>
         <iButton>取消组合</iButton>
         <iButton>导出</iButton>
@@ -43,7 +44,17 @@ export default{
   data(){return {
     title:title,
     exampelData:exampelData
-  }}
+  }},
+  methods: {
+    quote() {
+      this.$confirm('是否引用询价轮次报价?', '引用询价轮次报价', {
+        confirmButtonText: '是',
+        cancelButtonText: '否',
+      })
+      .then(() => {})
+      .catch(() => {});
+    }
+  }
 }
 </script>
 <style lang='scss' scoped>
@@ -57,7 +68,7 @@ export default{
         display: flex;
         justify-content: space-between;
         span{
-          width: 180px;
+          width: 160px;
           line-height: 30px;
           font-size: 14px;
         }
