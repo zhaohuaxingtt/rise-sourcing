768b960a16b1215c7258bcc8219e869d0683b651
周玉松 <yuszhou@deloitte.com.cn>
2021-06-08 20:20:41 +0800

    打开模具界面

--- src/views/partsrfq/editordetail/components/rfqDetailTpzs/components/quotationScoringMj/index.vue
+++ src/views/partsrfq/editordetail/components/rfqDetailTpzs/components/quotationScoringMj/index.vue
@@ -1,15 +1,15 @@
 <!--
  * @Author: yuszhou
  * @Date: 2021-05-27 14:55:03
- * @LastEditTime: 2021-05-31 14:35:42
+ * @LastEditTime: 2021-06-08 20:10:09
  * @LastEditors: Please set LastEditors
  * @Description: 采购员报价与基本分析模具界面
  * @FilePath: \front-web\src\views\partsrfq\editordetail\components\rfqDetailTpzs\components\quotationScoringMj\index.vue
 -->
 <script>
-// import quotationMj from 'rise/web/mouldOffer/components/moduleCost'
+import quotationMj from 'rise/web/mouldOffer/components/moduleCost'
 export default {
-  // extends:quotationMj,
+  extends:quotationMj,
   data(){
     return {
     useCardSlot:false,
