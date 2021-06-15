25bfe5e0d875b80f07f090f7883639ac70ccf47b
周玉松 <yuszhou@deloitte.com.cn>
2021-06-15 20:56:16 +0800

    cache code

--- src/views/partsrfq/editordetail/components/rfqDetailTpzs/components/quotationScoringMj/index.vue
+++ src/views/partsrfq/editordetail/components/rfqDetailTpzs/components/quotationScoringMj/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: yuszhou
  * @Date: 2021-05-27 14:55:03
- * @LastEditTime: 2021-06-14 14:21:31
+ * @LastEditTime: 2021-06-15 20:52:20
  * @LastEditors: Please set LastEditors
  * @Description: 采购员报价与基本分析模具界面
  * @FilePath: \front-web\src\views\partsrfq\editordetail\components\rfqDetailTpzs\components\quotationScoringMj\index.vue
@@ -9,21 +9,34 @@
 <script>
 import {getRfqSupplierList} from '@/api/designate/supplier'
 import quotationMj from 'rise/web/mouldOffer/components/moduleCost'
+import {iMessageBox} from 'rise'
 export default {
   extends:quotationMj,
   data(){
     return {
       useCardSlot:false,
       hasSupplierComponets:true,
-      supplierId:'11034',
+      supplierId:'',
       supplierList:[],
       myDisabled:true,
-      titleKey:"BJZS.LK_BAOJIAZUSHOU_MJ"
+      titleKey:"BJZS.LK_BAOJIAZUSHOU_MJ",
+      oldSupplierId:''
     }
   },
   computed:{
     disabled(){
       return !this.dgysBj
+    }
+  },
+  watch:{
+    /**
+     * @description: 记录上一次的供应商值
+     * @param {*} old
+     * @param {*} val
+     * @return {*}
+     */
+    "supplierId":function(old,val){
+      this.oldSupplierId = val
     }
   },
   created(){
@@ -36,8 +49,19 @@ export default {
   },
   methods:{
     getFee(res){
-      this.getAllMouldFee()
-      this.getAllPartForMould()
+      if(this.dgysBj && this.tableListData.length > 0){
+        iMessageBox("您确定要切换供应商吗？").then(res=>{
+          this.tableListData = []
+          this.getAllMouldFee()
+          this.getAllPartForMould()
+        }).catch(()=>{
+          this.supplierId = this.oldSupplierId
+        })
+      }else{
+        this.tableListData = []
+        this.getAllMouldFee()
+        this.getAllPartForMould()
+      }
     },
     getRfqSupplierList(){
      return new Promise(r=>{
