5beea60aa5fe972683c9a1e32f551c81040c6788
leeli <leeli@deloitte.com.cn>
2021-06-17 18:11:09 +0800

    月度

--- src/views/ws2/investmentAdmin/monthlyPlan/components/data.js
+++ src/views/ws2/investmentAdmin/monthlyPlan/components/data.js
@@ -1,87 +1,186 @@
 
 export const tableTitle = [
     {
-        props: 'tmCartypeProName',
+        props: 'department',
         name: '科室',
         key: 'LK_KESHI',
         tooltip: false
     },
     {
-        props: 'sopTime',
+        props: 'amount',
         name: '计划付款金额',
         key: '计划付款金额',
         tooltip: false
     },
     {
-        props: 'tmCartypeProName',
+        props: 'jan',
         name: '一月',
         key: '一月',
         tooltip: false
     },
     {
-        props: 'sopTime',
+        props: 'feb',
         name: '二月',
         key: '二月',
         tooltip: false
     },
     {
-        props: 'tmCartypeProName',
+        props: 'mar',
         name: '三月',
         key: '三月',
         tooltip: false
     },
     {
-        props: 'sopTime',
+        props: 'apr',
         name: '四月',
         key: '四月',
         tooltip: false
     },
     {
-        props: 'tmCartypeProName',
+        props: 'may',
         name: '五月',
         key: '五月',
         tooltip: false
     },
     {
-        props: 'sopTime',
+        props: 'june',
         name: '六月',
         key: '六月',
         tooltip: false
     },
     {
-        props: 'tmCartypeProName',
+        props: 'july',
         name: '七月',
         key: '七月',
         tooltip: false
     },
     {
-        props: 'sopTime',
+        props: 'aug',
         name: '八月',
         key: '八月',
         tooltip: false
     },
     {
-        props: 'tmCartypeProName',
+        props: 'sep',
         name: '九月',
         key: '九月',
         tooltip: false
     },
     {
-        props: 'sopTime',
+        props: 'oct',
         name: '十月',
         key: '十月',
         tooltip: false
     },
     {
-        props: 'tmCartypeProName',
+        props: 'nov',
         name: '十一月',
-        key: 'LK_KESHI',
+        key: '十一月',
         tooltip: false
     },
     {
-        props: 'sopTime',
+        props: 'dec',
         name: '十二月',
         key: '十二月',
         tooltip: false
     },
 ]
+
+export const dataList = [
+    {
+        department: "CSE",
+        amount: 5100,
+        jan: 300,
+        feb: 200,
+        mar: 300,
+        apr: 400,
+        may: 500,
+        june: 600,
+        july: 700,
+        aug: 800,
+        sep: 900,
+        oct: 300,
+        nov: 200,
+        dec: 300
+    },
+    {
+        department: "CSI",
+        amount: 5112,
+        jan: 301,
+        feb: 201,
+        mar: 301,
+        apr: 401,
+        may: 501,
+        june: 601,
+        july: 701,
+        aug: 401,
+        sep: 901,
+        oct: 301,
+        nov: 201,
+        dec: 301
+    },
+    {
+        department: "CSM",
+        amount: 5124,
+        jan: 302,
+        feb: 202,
+        mar: 302,
+        apr: 402,
+        may: 502,
+        june: 602,
+        july: 702,
+        aug: 802,
+        sep: 902,
+        oct: 302,
+        nov: 202,
+        dec: 302
+    },
+    {
+        department: "CSP",
+        amount: 5136,
+        jan: 303,
+        feb: 203,
+        mar: 303,
+        apr: 403,
+        may: 503,
+        june: 603,
+        july: 703,
+        aug: 803,
+        sep: 903,
+        oct: 303,
+        nov: 203,
+        dec: 303
+    },
+    {
+        department: "CSX",
+        amount: 5148,
+        jan: 304,
+        feb: 204,
+        mar: 304,
+        apr: 404,
+        may: 504,
+        june: 604,
+        july: 704,
+        aug: 804,
+        sep: 904,
+        oct: 304,
+        nov: 204,
+        dec: 304
+    },
+    {
+        department: "BU-B",
+        amount: 5160,
+        jan: 305,
+        feb: 205,
+        mar: 305,
+        apr: 405,
+        may: 505,
+        june: 605,
+        july: 705,
+        aug: 805,
+        sep: 905,
+        oct: 305,
+        nov: 205,
+        dec: 305
+    },
+];
