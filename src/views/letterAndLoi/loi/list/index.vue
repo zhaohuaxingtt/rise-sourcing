a96b7ebc2232c6a1810cf3c06a4670fc6f80e8bd
wentliao <wentliao@deloitte.com.cn>
2021-06-18 15:25:50 +0800

    新增定点信列表页

---
+++ src/views/letterAndLoi/loi/list/index.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: wentliao
+ * @Date: 2021-06-18 13:49:45
+ * @Description: LOI列表页
+-->
+<template>
+  <h1>LOI列表页</h1>
+</template>
+
+<script>
+export default {
+    name:'loiList'
+}
+</script>
+
+<style>
+
+</style>
