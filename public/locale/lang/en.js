ac5441f56c08684f2e7d2ec995c2b44725660ac4
周玉松 <yuszhou@deloitte.com.cn>
2021-06-25 00:59:47 +0800

    国际化修改

--- public/locale/lang/en.js
+++ public/locale/lang/en.js
@@ -993,13 +993,14 @@
         // eslint-disable-next-line no-undef
         xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState==4 && xmlHttp.status==200){
+            const data = JSON.parse(xmlHttp.responseText)
+            i18n.setLocaleMessage('en', Object.assign(oldLanguage,data.data.cn))
+        }
+    }
     xmlHttp.open("GET", "http://10.122.18.136:8088/i18n/getTranslationMap?from=sourcing",false);
     xmlHttp.setRequestHeader("Content-type","application/json");
     xmlHttp.setRequestHeader("Accept","*/*");
     xmlHttp.send()
-    xmlHttp.onreadystatechange = function(request) {
-        console.log(request)
-        i18n.setLocaleMessage('en', 
-        )
-    }
 }))
