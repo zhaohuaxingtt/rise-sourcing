c20986fe3b9cbae8e052711c89e9e2402f50bd89
wentliao <wentliao@deloitte.com.cn>
2021-07-03 20:55:07 +0800

    提交新增LOI

--- src/views/letterAndLoi/loi/detail/index.vue
+++ src/views/letterAndLoi/loi/detail/index.vue
@@ -12,7 +12,8 @@
                 <iButton :loading="btnLoading.save" @click="save">{{language('LK_BAOCUN','保存')}}</iButton>
                 <iButton @click="changeEditStatus">{{language('LK_QUXIAO','取消')}}</iButton>
             </span>
-            <span v-else>
+            <span v-else><!-- 状态为作废时显示 -->
+                <iButton :loading="btnLoading.add" @click="add" v-if="loiStatus=='TOVOID'">{{language('LK_LETTER_XINZENGLOI','新增LOI')}}</iButton>
                 <!-- 状态为前期确认中时显示编辑按钮 -->
                 <template  v-if="loiStatus=='CSF_HANDLING'" >
                     <iButton @click="edit">{{language('LK_BIANJI','编辑')}}</iButton>
@@ -102,6 +103,7 @@ import {
     exportTemplateLoi,
     confirmSubmitLio,
     cfsLoiDone,
+    addNomiLoi,
 } from '@/api/letterAndLoi/loi'
 import {
     getSupplierUsers,
@@ -138,6 +140,7 @@ export default {
                 lineBack:false,
                 save:false,
                 lineDone:false,
+                add:false,
             },
 
         }
@@ -325,6 +328,27 @@ export default {
                if(code ==200){
                    this.linieList = data;
                }
+            })
+        },
+
+        // 新增LOI
+        async add(){
+            const { loiInfo } = this;
+            const { id } = loiInfo;
+            const confirmInfo = await this.$confirm(this.language('submitSure','您确定要执行提交操作吗？'));
+            if(!confirmInfo) return;
+            this.btnLoading.add = true;
+            await addNomiLoi({id}).then((res)=>{
+                this.btnLoading.add = false;
+                const {code} = res;
+                if(code==200){
+                    iMessage.success(this.language('LK_CAOZUOCHENGGONG','操作成功'));
+                    this.getDetail();
+                }else{
+                    iMessage.error(this.$i18n.locale === "zh" ? res.desZh : res.desEn);
+                }
+            }).catch((err)=>{
+                this.btnLoading.add = false;
             })
         },
     }
