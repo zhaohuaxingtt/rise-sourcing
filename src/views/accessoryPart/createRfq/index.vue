89b70d14994578c4b3add4392730b82b0a67318f
舒杰 <jieshu@deloitte.com.cn>
2021-07-05 19:09:11 +0800

    add code

--- src/views/accessoryPart/createRfq/index.vue
+++ src/views/accessoryPart/createRfq/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: Luoshuang
  * @Date: 2021-05-26 13:54:01
- * @LastEditors: Luoshuang
- * @LastEditTime: 2021-06-25 13:55:33
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-07-02 18:12:17
  * @Description: 创建RFQ界面
        配件：选择的配件需要是分配了询价采购员的且是同一个询价采购员, 创建时能选择LINIE
        附件：选择的附件需要时分配了LINIE且为同一个LINIE, 创建时不能再选择LINIE
