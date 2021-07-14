4ac324db0620b37e394645e578c5ab7ec7885142
yuan <1132524215@qq.com>
2021-07-14 10:11:23 +0800

    bobRFQ检索条件禁用逻辑调整

--- src/views/partsrfq/bob/bob.vue
+++ src/views/partsrfq/bob/bob.vue
@@ -203,7 +203,8 @@ export default {
     //初始化查询数据
     initSearchData() {
       const data = this.$store.state.rfq.rfqId
-      if(data) this.rfqStatus = true
+      const status = this.$store.state.rfq.entryStatus
+      if(data && status == 0) this.rfqStatus = true
       this.form = {
         ...this.form,
         rfq: data
