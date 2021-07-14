d8001c6be095b0702433a4fa359ae3f580cb5019
姜浩 <543333675@qq.com>
2021-07-14 13:19:08 +0800

    零件项目类型配置文件

---
+++ src/config/index.js
@@ -0,0 +1,64 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-14 11:46:46
+ * @LastEditTime: 2021-07-14 13:17:52
+ * @LastEditors: Please set LastEditors
+ * @Description: 配置
+ * @FilePath: /front-web/src/config/index.js
+ */
+
+// 零件项目类型
+export const partProjectType = {
+  // DB零件一次性采购
+  DBLINGJIANYICIXCAIGOU: '50005000',
+  // DB一次性采购
+  DBYICHIXINGCAIGOU: '50001001',
+  // GS Common Sourcing
+  GSCOMMONSOURCING: '50002001',
+  // 工序委外
+  GONGXUWEIWAI: '1044297',
+  // Deep Localization
+  DEEPLOCALIZATION: '1044298',
+  // 一次性采购
+  YICIXINGCAIGOU: '50001000',
+  // AEKO零件(新)
+  AEKOLINGJIANXIN: '1000020',
+  // 钢材一次性采购
+  GANGCAIYICIXINGCAIGOU: '1000030',
+  // 扩产能
+  KUOCHANNENG: '50003001',
+  // DB涨价
+  DBZHANGJIA: '50003002',
+  // DB仅零件号更改
+  DBJINLINGJIANHAOGENGAI: '50003000',
+  // 涨价零件
+  ZHANGJIALINGJIAN: '50001150',
+  // CKD零件
+  CKDLINGJIAN: '1000008',
+  // DB零件
+  DBLINGJIAN: '1000009',
+  // FS小零件
+  FSXIAOLINGJIAN: '1000001',
+  // FS总成件
+  FSZONGCHENGJIAN: '1000002',
+  // GS零件
+  GSLINGJIAN: '1000003',
+  // COP零件
+  COPLINGJIAN: '1000004',
+  // 仅零件号更改
+  JINLINGJIANHAOGENGGAI: '1000005',
+  // AEKO零件
+  AEKOLINGJIAN: '1000006',
+  // SPECIAL零件
+  SPECIALLINGJIAN: '1000007',
+  // GS零件(TP)
+  GSLINGJIANIP: '50000000',
+  // FS Common Sourcing
+  FSCOMMONSOURCING: '50002000',
+  // 钢材批量采购
+  GANGCAIPILIANGCAIGOU: '1000040',
+  // 配件
+  PEIJIAN: '1000060',
+  // 附件
+  FUJIAN: '1000061'
+}
