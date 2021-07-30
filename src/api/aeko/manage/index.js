7378af93c3fbe3d04c13af87d9423922be88faed
wentliao <wentliao@deloitte.com.cn>
2021-07-30 13:31:57 +0800

    update aeko

--- src/api/aeko/manage/index.js
+++ src/api/aeko/manage/index.js
@@ -11,9 +11,36 @@ const requst = axios(process.env.VUE_APP_PARTS)
 
 // 列表查询
 export function getManageList(data) {
-    return requst({
-      url: '/aeko/purchasing/page',
-      method: 'POST',
-      data,
-    })
-  }
+  return requst({
+    url: '/aeko/purchasing/page',
+    method: 'POST',
+    data,
+  })
+}
+
+// aeko状态下拉框
+export function searchAekoStatus(params) {
+  return requst({
+    url: '/aeko/purchasing/search-aeko-status',
+    method: 'GET',
+    params,
+  })
+}
+
+// 品牌下拉框 
+export function searchBrand(params) {
+  return requst({
+    url: '/aeko/purchasing/search-brand',
+    method: 'GET',
+    params,
+  })
+}
+
+// 封面状态下拉框
+export function searchCoverStatus(params) {
+  return requst({
+    url: '/aeko/purchasing/search-cover-status',
+    method: 'GET',
+    params,
+  })
+}
