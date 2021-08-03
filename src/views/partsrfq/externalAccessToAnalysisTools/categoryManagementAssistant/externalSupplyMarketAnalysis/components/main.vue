083a84c762041a9f366f519214d3d6bfd86cfed2
yuan <1132524215@qq.com>
2021-08-03 20:18:37 +0800

    外部供应市场分析-总览

---
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/externalSupplyMarketAnalysis/components/main.vue
@@ -0,0 +1,67 @@
+<!--
+ * @Author: youyuan
+ * @Date: 2021-08-03 19:45:03
+ * @LastEditTime: 2021-08-03 20:10:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \front-web\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\externalSupplyMarketAnalysis\components\main.vue
+-->
+<template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <el-card class="box-card">
+          <p class="cardTitle">{{language('SVWGONGYINGSHANGSHICHANGZONGLAN', 'SVW供应商市场总览')}}</p>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="box-card">
+          <p class="cardTitle">{{language('HANGYEBAOGAO', '行业报告')}}</p>
+          
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="box-card">
+          <p class="cardTitle">{{language('SHICHANGSHUJU', '市场数据')}}</p>
+          
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="box-card">
+          <p class="cardTitle">{{language('GONGYINGLIANGAILAN', '供应链概览（N级供应链管理）')}}</p>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.box-card {
+  height: 320px;
+  margin: 10px 0;
+  cursor: pointer;
+  .cardTitle {
+    font-weight: bold;
+    font-size: 20px;
+  }
+}
+ 
+</style>
+
--- src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/externalSupplyMarketAnalysis/index.vue
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/externalSupplyMarketAnalysis/index.vue
@@ -1,17 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-07-30 15:47:49
+ * @LastEditTime: 2021-08-03 19:52:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \front-web\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\externalSupplyMarketAnalysis\index.vue
+-->
 <template>
   <iPage>
     <headerNav/>
-    外部供应市场分析
+    <mainContent/>
   </iPage>
 </template>
 
 <script>
 import {iPage} from 'rise'
 import headerNav from '../components/headerNav'
+import mainContent from './components/main'
 export default {
   components: {
     iPage,
-    headerNav
+    headerNav,
+    mainContent
   }
 };
 </script>
