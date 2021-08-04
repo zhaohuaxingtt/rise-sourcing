62716a9af8d9abc889e504fdedcefb35bdde0f62
yuan <1132524215@qq.com>
2021-08-04 16:22:56 +0800

    成本分析路由调整

--- src/router/modules/categoryManagementAssistant.js
+++ src/router/modules/categoryManagementAssistant.js
@@ -39,7 +39,31 @@ export default [{
           name: 'technology',
           meta: { title: "技术路线" },
           component: () => import('@/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/technology'),
-        }
+        },
+        {
+          path: 'costAnalysisMain',
+          name: 'costAnalysisMain',
+          meta: { title: "成本分析-系统筛选" },
+          component: () => import('@/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/costAnalysisMain'),
+        },
+        {
+          path: 'costAnalysis',
+          name: 'costAnalysis',
+          meta: { title: "成本分析库" },
+          component: () => import('@/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/costAnalysisMain/components/costAnalysis'),
+        },
+        {
+          path: 'costAnalysisAdd',
+          name: 'costAnalysisAdd',
+          meta: { title: "成本分析新增" },
+          component: () => import('@/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/costAnalysisMain/components/costAnalysisAdd'),
+        },
+        {
+          path: 'costAnalysisHandleInput',
+          name: 'costAnalysisHandleInput',
+          meta: { title: "手工输入" },
+          component: () => import('@/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/costAnalysisMain/components/costAnalysisHandleInput'),
+        },
       ]
     },
     {
