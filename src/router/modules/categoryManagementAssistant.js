79b309708b0d134c4034ab86233fdd1bfdfa5593
yuan <1132524215@qq.com>
2021-08-04 15:15:32 +0800

    外部市场分析概览页方式调整

--- src/router/modules/categoryManagementAssistant.js
+++ src/router/modules/categoryManagementAssistant.js
@@ -2,7 +2,7 @@
  * @version: 1.0
  * @Author: zbin
  * @Date: 2021-06-16 09:54:11
- * @LastEditors: 舒杰
+ * @LastEditors: Please set LastEditors
  * @Descripttion: your project
  */
 /*
@@ -46,7 +46,16 @@ export default [{
       //外部供应市场分析
       path: 'categoryManagementAssistant/externalSupplyMarketAnalysis',
       name: 'externalSupplyMarketAnalysis',
-      component: () => import('../../views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/externalSupplyMarketAnalysis')
+      redirect: 'categoryManagementAssistant/externalSupplyMarketAnalysis/overView',
+      component: () => import('../../views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/externalSupplyMarketAnalysis'),
+      children: [
+        {
+          path: 'overView',
+          name: 'overView',
+          meta: { title: "外部供应市场分析概览" },
+          component: () => import('@/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/externalSupplyMarketAnalysis/overView'),
+        },
+      ]
     },
     {
       //材料组定位
@@ -59,30 +68,6 @@ export default [{
       path: 'categoryManagementAssistant/listOfInitiatives',
       name: 'listOfInitiatives',
       component: () => import('../../views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/listOfInitiatives')
-    },
-    {
-      //开发阶段用——成本结构分析图-系统筛选
-      path: 'categoryManagementAssistant/costAnalysisMain',
-      name: 'costAnalysisMain',
-      component: () => import('../../views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/components/costAnalysisMain/index')
-    },
-    {
-      //开发阶段用——成本结构分析图
-      path: 'categoryManagementAssistant/costAnalysis',
-      name: 'costAnalysis',
-      component: () => import('../../views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/components/costAnalysisMain/components/costAnalysis/index')
-    },
-    {
-      //开发阶段用——成本结构数据添加
-      path: 'categoryManagementAssistant/costAnalysisAdd',
-      name: 'costAnalysisAdd',
-      component: () => import('../../views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/components/costAnalysisMain/components/costAnalysisAdd/index')
-    },
-    {
-      //开发阶段用——成本结构分析图-手工输入
-      path: 'categoryManagementAssistant/costAnalysisHandleInput',
-      name: 'costAnalysisHandleInput',
-      component: () => import('../../views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/components/costAnalysisMain/components/costAnalysisHandleInput/index')
     }
   ]
 }]
--- src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/externalSupplyMarketAnalysis/index.vue
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/externalSupplyMarketAnalysis/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 舒杰
  * @Date: 2021-08-03 10:42:23
- * @LastEditTime: 2021-08-04 14:37:06
+ * @LastEditTime: 2021-08-04 15:12:47
  * @LastEditors: Please set LastEditors
  * @Description: 外部供应市场分析
  * @FilePath: \front-sourcing\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\externalSupplyMarketAnalysis\index.vue
@@ -9,27 +9,17 @@
 <template>
   <iPage>
     <headerNav/>
-    <!-- 行业报告 -->
-    <!-- <industryReport></industryReport> -->
-    <!--    市场数据-->
-   <!-- <marketData class="margin-top30"/> -->
-    <mainContent/>
+    <router-view></router-view>
   </iPage>
 </template>
 
 <script>
 import {iPage} from 'rise'
 import headerNav from '../components/headerNav'
-import industryReport from './industryReport'
-import marketData from './marketData';
-import mainContent from './components/main'
 export default {
   components: {
     iPage,
     headerNav,
-    industryReport,
-    marketData,
-    mainContent
   }
 };
 </script>
