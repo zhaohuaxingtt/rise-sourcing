e3970dfecfe7254d8b915e65fdf90e0de57b41f3
舒杰 <jieshu@deloitte.com.cn>
2021-08-05 19:57:13 +0800

    add code

---
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/batchSupplier/index.vue
@@ -0,0 +1,185 @@
+<!--
+ * @Author: 舒杰
+ * @Date: 2021-08-05 16:27:57
+ * @LastEditTime: 2021-08-05 19:50:39
+ * @LastEditors: 舒杰
+ * @Description: 批量供应商概览
+ * @FilePath: \front-sourcing\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\internalDemandAnalysis\batchSupplier\index.vue
+-->
+<template>
+   <iCard class="margin-top20">
+      <template slot="header">
+         <div class="flex-between-center title">
+            <div class="flex">
+               <span>{{language("PLGYSGL","批量供应商概览")}}</span>
+               <el-tooltip content="Top center" placement="top" effect="light">
+                  <span class="mark">beizhu 大撒大撒但顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶水水水水水水水水水水水水水水</span>
+               </el-tooltip>
+            </div>
+            <div class="flex">
+               <iButton @click="save">{{ language("BEIZHU", "备注") }}</iButton>
+               <iButton @click="save">{{ language("BAOCUN", "保存") }}</iButton>
+               <iButton @click="back">{{ language("FANHUI", "返回") }}</iButton>
+            </div>
+         </div>
+		</template>
+      <!-- powerBi -->
+      <div id="powerBi"></div>
+   </iCard>
+</template>
+<script>
+import {iCard,iButton} from "rise";
+import {getCmSupplierPbi} from "@/api/categoryManagementAssistant/internalDemandAnalysis/batchSupplier";
+import * as pbi from 'powerbi-client';
+export default {
+   components:{iCard,iButton},
+   data () {
+      return {
+         filter : {
+            $schema: "http://powerbi.com/product/schema#basic",
+            target: {
+               table: "app_supplier_fin_analysis_sum_nt_daily",
+               column: "subject_name"
+            },
+            operator: "In",
+            values: [],//[this.name],// values
+            filterType: null,
+            requireSingleSelection: true
+         },
+         report:null,
+         name:"",
+         url: {
+            accessToken: "", //验证token
+            embedUrl: "", //报告信息内嵌地址
+            tokenExpiry: ""//token过期时间
+         },
+         values:[],
+         reportContainer:null
+      }
+   },
+   created () {
+      if (this.$route.query.name) {
+         this.name = this.$route.query.name;
+      }
+   },
+   mounted () {
+      this.filter={...this.filter,filterType:pbi.models.FilterType.BasicFilter},
+		this.getPowerBiUrl()
+   },
+   watch:{
+      '$i18n.locale':{
+         handler(newValue){
+         this.config.pageName=newValue=='zh'?'ReportSectione9fe87a027d2550c28a9':'ReportSection616eb7861df2ef50a3cd'
+         this.renderBi()
+      }}
+   },
+   methods: {
+      // 获取财报iframeurl
+      getPowerBiUrl() {
+            getCmSupplierPbi().then(res => {
+            if (res.data) {
+               this.url = res.data
+               this.init()
+               this.renderBi()
+            }
+         })
+      },
+      // 初始化配置
+      init(){
+         // this.permissions = pbi.models.Permissions.All
+         this.config = {
+            type: 'report',
+            tokenType: pbi.models.TokenType.Embed,
+            accessToken: this.url.accessToken,
+            embedUrl: this.url.embedUrl,
+            pageName:"ReportSectione9fe87a027d2550c28a9",// 中文ReportSectione9fe87a027d2550c28a9 英文 ReportSection616eb7861df2ef50a3cd
+            // id: 'f6bfd646-b718-44dc-a378-b73e6b528204',
+            // visualName: '47eb6c0240defd498d4b',
+            // permissions: permissions,
+            settings: {
+               panes: {
+                  filters: {
+                     visible: false
+                  },
+                  pageNavigation: {
+                     visible: false
+                  }
+               }
+            }
+         };
+         this.reportContainer = document.getElementById('powerBi');
+         this.powerbi = new pbi.service.Service(pbi.factories.hpmFactory, pbi.factories.wpmpFactory, pbi.factories.routerFactory);
+      },
+      renderBi() {
+         var report = this.powerbi.embed(this.reportContainer, this.config);
+         // Report.off removes a given event handler if it exists.
+         report.off("loaded");
+         // Report.on will add an event handler which prints to Log window.
+         const name = this.name
+         const newfilter = window._.cloneDeep(this.filter);
+         newfilter.values=[name]
+         this.values=[name]
+         console.log(newfilter);
+         report.on("loaded", ()=> {
+            console.log("Loaded");
+            // if(name==""){
+               // newfilter.values=[]
+            // report.updateFilters(pbi.models.FiltersOperations.Add, [newfilter]);
+            // }
+            report.setFilters([newfilter])
+         });
+         // Report.off removes a given event handler if it exists.
+         report.off("rendered");
+         // Report.on will add an event handler which prints to Log window.
+         report.on("rendered", function() {
+            console.log("Rendered");
+         });
+         // report.off("filtersApplied")
+         report.on("filtersApplied", function() {
+            console.log("filtersApplied");
+         });
+         report.on("error", function(event) {
+            report.off("error");
+         });
+         report.off("saved");
+         report.on("saved", function(event) {
+            if (event.detail.saveAs) {
+               console.log(
+                  'In order to interact with the new report, create a new token and load the new report'
+                  );
+            }
+         });
+         this.report=report
+      },
+      // 保存
+      save(){
+
+      },
+      // 返回
+      back(){
+         this.$router.go(-1)
+      }
+   }
+}
+</script>
+<style lang="scss" scoped>
+.title{
+   width: 100%;
+   .mark{
+      font-size: 14px !important;
+      opacity: 0.42;
+      margin-left: 32px;
+      font-weight: normal;
+      width: 590px;
+      @include text_;
+   }
+}
+.select{
+   width:120px;
+   margin-right:30px;
+}
+#powerBi {
+	width: 100%;
+	height: calc(100vh - 300px);
+}
+</style>
--- src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/technology/data.js
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/technology/data.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 舒杰
  * @Date: 2021-08-02 10:42:01
- * @LastEditTime: 2021-08-03 11:16:32
+ * @LastEditTime: 2021-08-05 15:27:51
  * @LastEditors: 舒杰
  * @Description: In User Settings Edit
  * @FilePath: \front-sourcing\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\internalDemandAnalysis\technology\data.js
@@ -9,8 +9,8 @@
 
 // 专项分析工具
 export const specialToolsTitle = [
-   { props: 'name', name: '文件名称', key: 'WENJIANMINGCHENG', tooltip: true },
-   { props: 'toolType', name: '年份', key: 'NIANFEN', tooltip: true },
-   { props: 'rfq', name: '创建人', key: 'CHUANGJIANREN', tooltip: true },
-   { props: 'type', name: '创建日期', key: 'CHUANGJIANRIQI', tooltip: true },
+   { props: 'fileName', name: '文件名称', key: 'WENJIANMINGCHENG', tooltip: true },
+   { props: 'year', name: '年份', key: 'NIANFEN', tooltip: true },
+   { props: 'userNameZh', name: '创建人', key: 'CHUANGJIANREN', tooltip: true },
+   { props: 'createDate', name: '创建日期', key: 'CHUANGJIANRIQI', tooltip: true },
 ]
