1fca8f3ce0efed6963d88f96a132d67838f7039b
zbin <bizhang@deloitte.com.cn>
2021-08-09 20:44:09 +0800

    feat 报告清单

--- src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/index.vue
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: 舒杰
  * @Date: 2021-08-03 10:42:23
- * @LastEditTime: 2021-08-09 15:33:42
- * @LastEditors: 舒杰
+ * @LastEditTime: 2021-08-09 19:17:49
+ * @LastEditors: zbin
  * @Description: 内部需求分析
  * @FilePath: \front-sourcing\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\internalDemandAnalysis\index.vue
 -->
@@ -11,22 +11,17 @@
     <headerNav>
       <div slot="extralButton">
         <iButton @click="openCategoryCode">{{ language("CAILIAOZU", "材料组") }}</iButton>
-        <iButton>{{ language("BAOGAOQINGDAN", "报告清单") }}</iButton>
+        <iButton @click="handleReport">{{ language("BAOGAOQINGDAN", "报告清单") }}</iButton>
         <iButton>{{ language("CHAKANLISHI", "查看历史") }}</iButton>
       </div>
     </headerNav>
     <router-view></router-view>
+    <reportInventory v-model="reportInventory" />
     <!-- 材料组定位 -->
     <iDialog :title='language("CAILIAOZU", "材料组")' :visible.sync="value" class="iDialog" width="400px" @close='clearDiolog' top="40vh">
       <p class="tip margin-bottom10">{{ language("QXZCAILIAOZU", "请选择材料组") }}</p>
       <!-- <iInput v-model="categoryCode"></iInput> -->
-      <el-autocomplete
-        class="autocomplete"
-        v-model="categoryName"
-        :fetch-suggestions="querySearchAsync"
-        placeholder="请输入内容"
-        @select="handleSelect"
-      ></el-autocomplete>
+      <el-autocomplete class="autocomplete" v-model="categoryName" :fetch-suggestions="querySearchAsync" placeholder="请输入内容" @select="handleSelect"></el-autocomplete>
       <span slot="footer" class="dialog-footer">
         <iButton @click="confirm">{{ language("QUEREN", "确认") }}</iButton>
       </span>
@@ -35,36 +30,43 @@
 </template>
 
 <script>
-import { iPage,iButton,iDialog} from 'rise';
+import { iPage, iButton, iDialog } from 'rise';
 import headerNav from '../components/headerNav';
 import { pageRfqBaseInfo } from "@/api/partsrfq/specialAnalysisTool/specialAnalysisTool";
+import reportInventory from "@/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/reportInventory/index.vue";
 export default {
   components: {
     iPage,
     headerNav,
     iButton,
-    iDialog
+    iDialog,
+    reportInventory
   },
-  data () {
+  data() {
     return {
-      value:false,
-      categoryName:"",//材料组名称
-      categoryCode:"",//材料组编号
+      value: false,
+      categoryName: "",//材料组名称
+      categoryCode: "",//材料组编号
+      reportInventory: false
     }
   },
-  created () {
-    if(!this.$store.state.rfq.categoryCode){
-      this.value=true
+  created() {
+    if (!this.$store.state.rfq.categoryCode) {
+      this.value = true
     }
   },
   methods: {
-    querySearchAsync(queryString, cb){
-      pageRfqBaseInfo({keyword:queryString}).then(res=>{
-        if(res.data){
-        let result=[]
-          res.data.map(item=>{
-          if(item.categoryCode){
-             result.push({value:item.categoryName,categoryCode:item.categoryCode})
+    handleReport() {
+      console.log(23456789);
+      this.reportInventory = true
+    },
+    querySearchAsync(queryString, cb) {
+      pageRfqBaseInfo({ keyword: queryString }).then(res => {
+        if (res.data) {
+          let result = []
+          res.data.map(item => {
+            if (item.categoryCode) {
+              result.push({ value: item.categoryName, categoryCode: item.categoryCode })
             }
           })
           cb(result)
@@ -72,23 +74,23 @@ export default {
       })
     },
     // 确认
-    confirm(){
-      this.$store.dispatch('setCategoryCode',this.categoryCode)
-      this.value=false
+    confirm() {
+      this.$store.dispatch('setCategoryCode', this.categoryCode)
+      this.value = false
     },
     // 重新定位材料组
-    openCategoryCode(){
-      this.value=true
+    openCategoryCode() {
+      this.value = true
     },
-    handleSelect(item){
-      this.categoryCode=item.categoryCode
+    handleSelect(item) {
+      this.categoryCode = item.categoryCode
     }
   }
 };
 </script>
 
 <style scoped lang="scss">
-.autocomplete{
+.autocomplete {
   width: 100%;
 }
 </style>
