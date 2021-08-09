38bc03e7da7e7369f58a6975e5642e4881ccd6ab
Zhang <550643175@qq.com>
2021-08-09 11:36:35 +0800

    权限限制

--- src/views/ws2/purchaseSupplier/mouldBook/components/data.js
+++ src/views/ws2/purchaseSupplier/mouldBook/components/data.js
@@ -6,8 +6,8 @@ export const bmForm = {
   sapOrder: '', //  SAP订单号
   enterAccountNum: '', //  入账单号
   tmCartypeProId: '',  //  车型项目
-  designatedSupplierId: [],  //  供应商id数组
-  deptId: '',  //  科室
+  designatedSupplierId: '',  //  供应商id数组
+  deptId: [],  //  科室
   linieId: [],  //  line
   moldInvestmentStatus: '', //  摸具投资状态
 }
