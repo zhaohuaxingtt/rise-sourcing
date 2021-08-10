57038378840b61ed024abb6c0211931f2b3a3115
EXT15805 <Csvw123456>
2021-08-10 10:49:09 +0800

    S12 缺陷修改

--- src/views/ws2/purchase/investmentList/index.vue
+++ src/views/ws2/purchase/investmentList/index.vue
@@ -125,7 +125,7 @@
             }}</div>
         </template>
         <template #moldInvestmentAmount="scope">
-          <div v-if="Number(isShowMoldInvestmentAmount) === 1">{{getTousandNum(Number(scope.row.moldInvestmentAmount).toFixed(2))}}</div>
+          <div v-if="Number(scope.row.isShowMoldInvestmentAmount) === 1">{{getTousandNum(Number(scope.row.moldInvestmentAmount).toFixed(2))}}</div>
           <div v-else>-</div>
         </template>
         <template #moldInvestmentStatus="scope">
