08a71f435eb2e69d3ae6101b376f05f32f865d68
姜浩 <543333675@qq.com>
2021-08-13 22:27:13 +0800

    寻源概览修正因为配置导致页面打不开的情况

--- src/views/dashboard/components/data.js
+++ src/views/dashboard/components/data.js
@@ -1,7 +1,7 @@
 /*
  * @Author: hoajiang
  * @Date: 2021-08-05 10:36:42
- * @LastEditTime: 2021-08-11 11:01:21
+ * @LastEditTime: 2021-08-13 22:13:31
  * @LastEditors: Please set LastEditors
  * @Description: 寻源概览配置
  * @FilePath: /front-web/src/views/dashboard/components/data.js
@@ -10,7 +10,7 @@
 // 采购员，股长，科长可见
 export const TAB = [
   {
-    value: 0,
+    value: 1,
     name: "寻源概览",
     message: 0,
     url: "/sourcing/dashboard",
@@ -18,7 +18,7 @@ export const TAB = [
     key: "XUANYUANGAILAN"
   },
   {
-    value: 1,
+    value: 2,
     name: "寻源执行",
     message: 0,
     url: "/sourcing/partsign",
@@ -26,7 +26,7 @@ export const TAB = [
     key: "XUANYUANCAIDAN"
   },
   {
-    value: 1,
+    value: 3,
     name: "报表管理",
     message: 0,
     url: "/sourcing/reportmgmt/report",
