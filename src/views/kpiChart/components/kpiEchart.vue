7b2a68783a3c3a41a33dfab9113b3756f04ac38e
wangwei <wei>
2021-08-17 21:00:43 +0800

    add

--- src/views/kpiChart/components/kpiEchart.vue
+++ src/views/kpiChart/components/kpiEchart.vue
@@ -125,9 +125,10 @@ export default {
             handler(curVal,oldVal){
                 this.seriesObj=this.options.series[0]
                 this.option.tooltip.formatter=function(params){
+                    console.log(params)
                         const str = `<div style="padding:10px">
                             <div>该分数断下供应商数量:<span style="color:#1763F7">${params.value}家</span></div>
-                            <div>${params.seriesName}:${params.name}分</div>
+                            <div>${params.data.name}:${params.name}分</div>
                         </div>`
                         return str
                     }
