9e41ef5782101e56b6a1a791b7a826d27465f16d
舒杰 <jieshu@deloitte.com.cn>
2021-08-23 20:49:28 +0800

    add 人工成本维护

---
+++ src/views/costanalysismanage/components/datamaintenance/components/costMaintenance/dateSelect.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Author: yuszhou
+ * @Date: 2021-02-25 11:24:15
+ * @LastEditTime: 2021-08-23 20:47:25
+ * @LastEditors: 舒杰
+ * @Description: In User Settings Edit
+ * @FilePath: \front-sourcing\src\views\costanalysismanage\components\datamaintenance\components\costMaintenance\dateSelect.vue
+-->
+<template>
+      <iDialog :title="language('SHANGCHUAN','上传')" :visible.sync="value" width="400px" @close='clearDiolog' top="40vh">
+        <p class="tip margin-bottom10">{{ language("QXZCAILIAOZU", "请选择将要上传的人工成本所属年份") }}</p>
+        <iDatePicker  type="year" v-model="date" value-format="yyyy" :picker-options="pickerOptions" ></iDatePicker>
+        <span slot="footer" class="dialog-footer">
+          <iButton :loading='repeatClick' @click="sureChangeItems">{{language('LK_QUEREN','确认')}}</iButton>
+        </span>
+      </iDialog>
+</template>
+<script>
+import {iButton,iDatePicker,iDialog} from 'rise'
+export default{
+  components:{iButton,iDatePicker,iDialog},
+  props:{
+    value:{type:Boolean},
+  },
+  data(){
+      return {
+        date:"",
+        pickerOptions: {
+         disabledDate(time) {
+            let currentYear = new Date().getFullYear()
+            return time.getFullYear() !== currentYear && time.getFullYear() !== currentYear - 1 && time.getFullYear() !== currentYear - 2;
+         }
+      },
+    }
+  },
+  methods:{
+    clearDiolog(){
+      this.$emit('input',false)
+    },
+    sureChangeItems(){
+      this.$emit('sure',this.date)
+    }    
+  }
+}
+</script>
+<style lang='scss' scoped>
+  
+</style>
--- src/views/costanalysismanage/components/datamaintenance/components/data.js
+++ src/views/costanalysismanage/components/datamaintenance/components/data.js
@@ -1,10 +1,10 @@
 /*
  * @Author: your name
  * @Date: 2021-05-27 13:50:49
- * @LastEditTime: 2021-06-25 14:05:31
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-08-23 15:25:25
+ * @LastEditors: 舒杰
  * @Description: In User Settings Edit
- * @FilePath: \front-web\src\views\costanalysismanage\components\datamaintenance\components\data.js
+ * @FilePath: \front-sourcing\src\views\costanalysismanage\components\datamaintenance\components\data.js
  */
 export const tableTitle = [
   { props: "fileName", name: "上传文件名称", key: "SHANGCHUANWENJIANMINGCHENG", tooltip: true },
@@ -12,4 +12,28 @@ export const tableTitle = [
   { props: "uploadDate", name: "时间", key: "SHIJIAN", tooltip: true },
   { props: "uploadBy", name: "上传人", key: "SHANGCHUANREN", tooltip: true },
   // { props: "remark", name: "备注", key: "costanalysismanage.BeiZhu", tooltip: true }
+]
+
+export const navList= [
+  {
+    value: 1,
+    name: "人工成本维护",
+    url: "/costanalysismanage/datamaintenance/costMaintenance",
+    activePath: "/costanalysismanage/datamaintenance/costMaintenance",
+    key: "RENGONGCHENGBENWEIHU"
+  },
+  {
+    value: 2,
+    name: "人工成本数据维护",
+    url: "/costanalysismanage/datamaintenance/costDataMaintenance",
+    activePath: "/costanalysismanage/datamaintenance/costDataMaintenance",
+    key: "RGCBSJWH"
+  },
+  {
+    value: 3,
+    name: "Heavy Item清单维护",
+    url: "/costanalysismanage/datamaintenance/heavyItem",
+    activePath: "/costanalysismanage/datamaintenance/heavyItem",
+    key: "HEAVYITEMQDWH"
+  }
 ]
