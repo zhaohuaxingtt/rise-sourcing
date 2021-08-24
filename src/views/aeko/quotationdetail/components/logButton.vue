a80c8a7508235f4f832e89b6092a30f210c8107c
ldh <diliu@deloitte.com.cn>
2021-08-24 18:52:22 +0800

    update code

---
+++ src/views/aeko/quotationdetail/components/logButton.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Author: ldh
+ * @Date: 2021-04-21 17:14:24
+ * @LastEditors: ldh
+ * @LastEditTime: 2021-04-23 14:17:32
+ * @Description: In User Settings Edit
+ * @FilePath: \front-supplier\src\views\rfqManageMent\quotationdetail\components\logButton.vue
+-->
+<template>
+  <div class="logButton" @click="$emit('click')">
+    <icon symbol name="iconrizhiwuzi" class="icon" />
+    <span>{{ $t("LK_RIZHI") }}</span>
+  </div>
+</template>
+
+<script>
+import { icon } from "rise";
+
+export default {
+  components: { icon },
+};
+</script>
+
+<style lang="scss" scoped>
+.logButton {
+  display: inline-block;
+  user-select: none;
+  cursor: pointer;
+
+  .icon {
+    width: 20px;
+    height: 20px;
+    vertical-align: top;
+
+    & + span {
+      margin-left: 4px;
+    }
+  }
+
+  span {
+    display: inline-block;
+    font-size: 14px;
+    height: 20px;
+    line-height: 20px;
+    color: $color-blue;
+  }
+}
+</style>
