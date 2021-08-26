3c6d802adb408a81affb2be3497042a9dba9af81
wentliao <wentliao@deloitte.com.cn>
2021-08-26 16:32:20 +0800

    update 指定原零件页面

--- src/views/aeko/quotationdetail/components/sampleFee/index.vue
+++ src/views/aeko/quotationdetail/components/sampleFee/index.vue
@@ -16,6 +16,37 @@
           </div>
         </div>
       </template>
+      <div>
+        <tableList
+          class="table"
+          :lang="true"
+          :tableData="tableListData"
+          :tableTitle="tableTitle"
+          :tableLoading="loading"
+          :selection="false"
+          @handleSelectionChange="handleSelectionChange"
+        >
+          <!-- ⼯装样件单件费⽤ -->
+          <template #b="scope">
+            <iInput class="tableInput" v-model="scope.row['b']" />
+          </template>
+          
+          <!-- 附加⼯装样件模具价格 -->
+          <template #c="scope">
+            <iInput class="tableInput" v-model="scope.row['c']" />
+          </template>
+          
+          <!-- 附加⼯装样件模具寿命 -->
+          <template #d="scope">
+            <iInput class="tableInput" v-model="scope.row['d']" />
+          </template>
+
+          <!-- 备注 -->
+          <template #e="scope">
+            <iInput class="tableInput" v-model="scope.row['e']" />
+          </template>
+        </tableList>
+      </div>
   </iCard>
 </template>
 
@@ -23,11 +54,31 @@
 import {
   iCard,
   iButton,
+  iInput,
 } from 'rise'
+import {
+  sampleFeeTableTitle as tableTitle,
+} from '../data'
+import tableList from "@/views/partsign/editordetail/components/tableList"
+
 export default {
   components:{
     iCard,
     iButton,
+    tableList,
+    iInput,
+  },
+  data(){
+    return{
+      tableTitle,
+      tableListData:[
+        {a:'⼯装样件(BS1)',b:'',c:'',d:'',e:''},
+        {a:'⼯装样件(BS2)',b:'',c:'',d:'',e:''},
+        {a:'⼯装样件(BS3)',b:'',c:'',d:'',e:''},
+        {a:'其他样件',b:'',c:'',d:'',e:''},
+      ],
+      loading:false,
+    }
   }
 }
 </script>
@@ -47,6 +98,9 @@ export default {
         font-weight: bold;
         color: #131523;
       }
+    }
+    .tableInput{
+      width: 70%;
     }
   }
 </style>
