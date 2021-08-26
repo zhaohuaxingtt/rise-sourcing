9e7cf363c299cdc2f75b919bde9281ba3767439f
wentliao <wentliao@deloitte.com.cn>
2021-08-26 10:57:28 +0800

    update aeko

--- src/views/aeko/quotationdetail/components/developmentFee/index.vue
+++ src/views/aeko/quotationdetail/components/developmentFee/index.vue
@@ -1,10 +1,26 @@
+<!--
+ * @Author: wentliao
+ * @Date: 2021-08-25 09:56:39
+ * @Description: 
+-->
 <template>
-  <div></div>
+  <div>
+    <mouldAndDevelopmentCost @save="save" :isAeko="true"/>
+  </div>
 </template>
 
 <script>
+import mouldAndDevelopmentCost from 'rise/web/quotationdetail/components/mouldAndDevelopmentCost/components/developmentCost'
 export default {
-
+  components:{
+    mouldAndDevelopmentCost,
+  },
+  methods:{
+    // 保存
+    save(value){
+      console.log(value,'savesavesave');
+    }
+  }
 }
 </script>
 
