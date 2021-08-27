d28d797dd175be77819860a657f8c40a57b08b4b
舒杰 <jieshu@deloitte.com.cn>
2021-08-27 17:34:16 +0800

    add code

--- vue.config.js
+++ vue.config.js
@@ -363,8 +363,7 @@ module.exports = {
         },
       },
       '/aonApi': {
-        // target: 'http://10.122.18.166:8036',  
-        target: 'http://10.160.138.20:8036',
+        target: 'http://10.122.18.166:8036',  
         // target: 'http://10.122.17.38:8036',
         changeOrigin: true,
         pathRewrite: {
