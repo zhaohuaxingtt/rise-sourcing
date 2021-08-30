ad0281c07c4212fe6ee9bb4118c65e9558c8614b
姜浩 <543333675@qq.com>
2021-08-30 17:12:35 +0800

    projectAPI dev

--- vue.config.js
+++ vue.config.js
@@ -116,6 +116,7 @@ module.exports = {
     proxy: {
       '/projectApi': {
         target: 'http://10.122.17.38:8005',
+        // target: 'http://10.122.18.166:8005', //dev
         // target: 'http://10.160.141.116:10085',
         changeOrigin: true,
         pathRewrite: {
