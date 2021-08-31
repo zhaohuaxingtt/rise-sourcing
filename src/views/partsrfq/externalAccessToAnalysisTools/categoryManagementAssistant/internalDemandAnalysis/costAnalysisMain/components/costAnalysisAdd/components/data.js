15b1af5931761db8c3fae2118e87964162c12963
yuan <1132524215@qq.com>
2021-08-31 18:39:01 +0800

    fix bug

--- src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/costAnalysisMain/components/costAnalysisAdd/components/data.js
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/costAnalysisMain/components/costAnalysisAdd/components/data.js
@@ -1,13 +1,13 @@
 /*
  * @Author: youyuan
  * @Date: 2021-08-03 10:44:04
- * @LastEditTime: 2021-08-19 14:59:52
+ * @LastEditTime: 2021-08-31 18:38:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \front-web\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\internalDemandAnalysis\components\costAnalysisMain\components\costAnalysisAdd\components\data.js
  */
 export const tableTitle = [
-  {props: 'partNameZh', name: '零件号', key: 'LINGJIANHAO'},
+  {props: 'partNum', name: '零件号', key: 'LINGJIANHAO'},
   {props: 'fsNum', name: 'FS号', key: 'FSHAO'},
   {props: 'supplierName', name: '供应商', key: 'GONGYINGSHANG'},
   {props: 'linie', name: 'Linie', key: 'LINIEXIAOXIE'},
