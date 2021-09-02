25fa0e9038c4553f383ca1d2b2fa338251ed4a21
周玉松 <yuszhou@deloitte.com.cn>
2021-09-02 16:35:25 +0800

    修改rfq

--- src/config/businesskey/moudules/1000006.js
+++ src/config/businesskey/moudules/1000006.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-25 19:03:24
- * @LastEditTime: 2021-08-27 14:52:47
+ * @LastEditTime: 2021-09-02 16:34:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \front-web\src\config\businesskey\moudules\aeko.js
