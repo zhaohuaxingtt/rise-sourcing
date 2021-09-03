0d209b8594b6eadf49cd1e70de184ee9e36144ab
姜浩 <543333675@qq.com>
2021-09-03 09:15:44 +0800

    文件处理相关API更新

--- src/api/file/index.js
+++ src/api/file/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-04-12 23:48:38
- * @LastEditTime: 2021-08-19 12:55:24
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-09-02 10:42:24
+ * @LastEditors: Hao,Jiang
  * @Description: In User Settings Edit
  * @FilePath: \front-web\src\api\file\index.js
  */
@@ -11,7 +11,7 @@ import { serialize } from '@/utils'
 import httpAxios from '@/utils/axios'
 const requst = axios(process.env.VUE_APP_COMMON)
 const fileRequst = axios(process.env.VUE_APP_NEW_COMMON)
-const sourcing = httpAxios(process.env.VUE_APP_SOURCING_WDL)
+const sourcing = httpAxios(process.env.VUE_APP_RFQ)
 export function downloadFile(parmars) {
     return requst({
         url: `/download?applicationName=${parmars.applicationName}&fileList=${parmars.fileList}`,
@@ -48,5 +48,30 @@ export function deleteFiles(params) {
         url: `/file-histories/delete-files`,
         method: 'DELETE',
         data:params
+    })
+}
+
+//sourcing 更新文件排序
+export function updateSort(data) {
+    return sourcing({
+        url: `/file-histories/update-sort/${data.fileId}/${data.isUp}`,
+        method: "PATCH",
+        data
+    })
+}
+
+/**
+ * @description: sourcing 上传文件（CF上传完业务回调）
+ * 上传分为2步，
+ * 第一步，调用CF接口上传至OSS
+ * 第二部，根据返回的uploadId，调业务回调接口完成上传
+ * @param {*} data
+ * @return {*}
+ */
+export function uploadfile(data) {
+    return sourcing({
+        url: `/file-histories/upload-file`,
+        method: "POST",
+        data
     })
 }
