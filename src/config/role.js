59b2d14a3fcff2a62bbe25d3eeada40207e72c27
姜浩 <543333675@qq.com>
2021-09-03 16:35:04 +0800

    角色mixin

--- src/config/role.js
+++ src/config/role.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-16 14:41:48
- * @LastEditTime: 2021-09-03 15:34:36
+ * @LastEditTime: 2021-09-03 16:02:59
  * @LastEditors: Hao,Jiang
  * @Description: 角色权限判断,配置根据portal角色管理配置而来
  * @FilePath: /front-web/src/config/dashboard.js
@@ -15,4 +15,9 @@ export const qianqicaigou = [
 // 专业采购员
 export const zhuanyecaigou = [
   'ZYCGY','ZYCGKZ','ZYCGGZ','WS1ZYCGY','WS1ZYCGKZ','WS1ZYCGGZ','WS2ZYCGY','WS2ZYCGKZ','WS2ZYCGGZ'
+]
+
+// 定点管理员
+export const dingdianguanli = [
+  'WS1DA','DDGLY'
 ]
