b616f517a56a61c0e17ea91677788207718131b4
yuan <1132524215@qq.com>
2021-09-08 14:33:33 +0800

    fix bug

--- src/views/partsrfq/vpAnalyse/vpAnalyseList/components/analysisTable.vue
+++ src/views/partsrfq/vpAnalyse/vpAnalyseList/components/analysisTable.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: youyuan
  * @Date: 2021-06-16 20:44:29
- * @LastEditTime: 2021-09-03 16:12:25
+ * @LastEditTime: 2021-09-08 11:15:59
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \front-web\src\views\partsrfq\vpAnalyse\vpAnalyseList\components\analysisTable.vue
