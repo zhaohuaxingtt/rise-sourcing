2d0285452f9591f9975ddb18184b24f10f5bac7a
姜浩 <543333675@qq.com>
2021-09-08 10:32:49 +0800

    排程版本下载

--- src/views/project/schedulingassistant/scheduleVersion/index.vue
+++ src/views/project/schedulingassistant/scheduleVersion/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: Luoshuang
  * @Date: 2021-07-27 14:30:02
- * @LastEditors: Luoshuang
- * @LastEditTime: 2021-09-07 17:16:12
+ * @LastEditors: Hao,Jiang
+ * @LastEditTime: 2021-09-08 10:29:37
  * @Description: 排程版本查询
  * @FilePath: \front-web\src\views\project\schedulingassistant\scheduleVersion\index.vue
 -->
@@ -17,6 +17,7 @@
         <div class="floatright">
           <!-- 批量下载排程版本 -->
           <iButton
+            :loading="batchUploading"
             @click="batchDownload"
           >
             {{ language('LK_XIAZAI', '下载') }}
@@ -63,7 +64,8 @@ export default {
       tableTitle,
       tableData: [],
       selectTableData: [],
-      tableLoading: false
+      tableLoading: false,
+      batchUploading: false
     }
   },
   mounted() {
@@ -128,7 +130,7 @@ export default {
           }])
         return
       }
-      downloadFile(row.id)
+      downloadFile(row.fileId)
     },
     batchDownload() {
       const fileList = this.selectTableData.map(o => {
@@ -141,6 +143,7 @@ export default {
         iMessage.error(this.language('QINGXUANZEZHISHAOYITIAOSHUJU','请选择至少一条数据'))
         return
       }
+      this.batchUploading = true
       this.genScheduleVersion(fileList)
     },
     /**
@@ -148,18 +151,24 @@ export default {
      * @param {*} file 字符串表示单个文件下载
      * @return {*}
      */   
-    genScheduleVersion(fileList) {
-      genScheduleVersionFileId(fileList).then(res => {
+    async genScheduleVersion(fileList) {
+      try {
+        const res = await genScheduleVersionFileId(fileList)
         if (res.code === '200') {
-          console.log(res)
+          const fileList = res.data || []
+          if (fileList.length) {
+            downloadFile(fileList.map(o => o.id))
+          }
         } else {
           iMessage.error(this.$i18n.locale === "zh" ? res.desZh : res.desEn)
         }
-      }).catch(e => {
+        this.batchUploading = false
+      } catch (e) {
+        this.batchUploading = false
         iMessage.error(this.$i18n.locale === "zh" ? e.desZh : e.desEn)
-      })
+      }
+      this.batchUploading = false
     }
-
   }
   
 }
