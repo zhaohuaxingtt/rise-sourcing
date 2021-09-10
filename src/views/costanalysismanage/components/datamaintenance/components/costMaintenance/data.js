6d253859bd400a4cbbdda2971e1b9ee90babf760
舒杰 <jieshu@deloitte.com.cn>
2021-09-10 10:24:35 +0800

    fix bug

--- src/views/costanalysismanage/components/datamaintenance/components/costMaintenance/data.js
+++ src/views/costanalysismanage/components/datamaintenance/components/costMaintenance/data.js
@@ -1,15 +1,15 @@
 /*
  * @Author: 舒杰
  * @Date: 2021-08-23 16:12:07
- * @LastEditTime: 2021-08-23 17:23:34
+ * @LastEditTime: 2021-09-10 10:22:56
  * @LastEditors: 舒杰
  * @Description: In User Settings Edit
  * @FilePath: \front-sourcing\src\views\costanalysismanage\components\datamaintenance\components\costMaintenance\data.js
  */
 export const tableTitle = [
-   { props: "year", name: "年份", key: "NIANFEN", tooltip: true },
-   { props: "fileName", name: "上传文件名称", key: "SHANGCHUANWENJIANMINGCHENG", tooltip: true },
-   { props: "fileSize", name: "大小（MB）", key: "DAXIAOMB", tooltip: true },
-   { props: "createDate", name: "时间", key: "SHIJIAN", tooltip: true },
-   { props: "createUserName", name: "上传人", key: "SHANGCHUANREN", tooltip: true },
+  { props: "year", name: "年份", key: "NIANFEN", tooltip: true },
+  { props: "fileName", name: "文件名称", key: "WENJIANMINGCHENG", tooltip: true },
+  { props: "fileSize", name: "大小（MB）", key: "DAXIAOMB", tooltip: true },
+  { props: "createUserName", name: "上传人", key: "SHANGCHUANREN", tooltip: true },
+  { props: "createDate", name: "上传时间", key: "SHANGCHUANSHIJIAN", tooltip: true },
  ]
