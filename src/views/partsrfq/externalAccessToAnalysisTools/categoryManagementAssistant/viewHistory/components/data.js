52ed60dfe20fee1100267e8c761c3c899a86f2da
zbin <bizhang@deloitte.com.cn>
2021-09-13 19:17:47 +0800

    fxibug

--- src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/viewHistory/components/data.js
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/viewHistory/components/data.js
@@ -3,13 +3,12 @@
  * @Author: zbin
  * @Date: 2021-05-21 10:18:28
  * @LastEditors: zbin
- * @LastEditTime: 2021-08-10 16:10:19
  * @Descripttion: your project
  */
 export const tableTitle = [
-  { props: 'categoryName', name: '材料组名称', key: 'CAILIAOZUMINGCHENG' , tooltip: true },
-  { props: 'years', name: '年份', key: 'NIANFEN' , tooltip: true },
-  { props: 'reportName', name: '报告名称', key: 'BAOGAOMINGCHENG'  },
-  { props: 'createDate', name: '创建日期', key: 'CHUANGJANRIQI',  tooltip: true },
-  { props: 'nameZh', name: '创建人', key: 'CHUANGJIANSHIJIAN',  tooltip: true },
+  { props: 'categoryName', name: '材料组名称', key: 'CAILIAOZUMINGCHENG' , tooltip: true ,width:200},
+  { props: 'years', name: '年份', key: 'NIANFEN' , tooltip: true ,width:100},
+  { props: 'reportName', name: '报告名称', key: 'BAOGAOMINGCHENG',tooltip: true  },
+  { props: 'createDate', name: '创建日期', key: 'CHUANGJANRIQI',  tooltip: true ,width:100},
+  { props: 'nameZh', name: '创建人', key: 'CHUANGJIANSHIJIAN',  tooltip: true ,width:100},
 ]
