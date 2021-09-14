85250d144fd6b2b809229800d8711af9d678b294
周玉松 <yuszhou@deloitte.com.cn>
2021-09-14 12:08:05 +0800

    修改bug supplierName 表头不展示

--- src/components/partsprocure/createNomiappBtnAsse/data.js
+++ src/components/partsprocure/createNomiappBtnAsse/data.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-09-03 16:27:49
- * @LastEditTime: 2021-09-03 16:50:47
+ * @LastEditTime: 2021-09-14 12:02:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \front-web\src\components\partsprocure\createNomiappBtnAsse\data.js
  */
 export const tableTitle = [
-  {props:'supplierName',name:'',key: 'LK_FSNRGSNRNUMBER',tooltip:false,width:''},
-  {props:'fsnrGsnrNum',name:'FS号',key: 'LK_FSNRGSNRNUMBER',tooltip:false,width:''},
+  {props:'supplierName',name:'',key: '',tooltip:false,width:''},
+  {props:'fsnrGsnrNum',name:'',key: 'LK_FSNRGSNRNUMBER',tooltip:false,width:''},
   {props:'sapNum',name:'供应商SAP号',key: 'LK_SAPNUMBER',tooltip:false,width:''},
   {props:'procureFactoryName',name:'采购工厂',key: 'LK_CAIGOUGONGC',tooltip:false,width:''},
   {props:'partNum',name:'零件号',key: 'LK_LINGJIANHAO',tooltip:false,width:''},
