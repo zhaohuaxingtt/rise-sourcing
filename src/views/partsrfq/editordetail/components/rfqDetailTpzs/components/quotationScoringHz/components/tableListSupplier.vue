0472dbdfc6e7057a3d08a0bd24967140cb20c538
周玉松 <yuszhou@deloitte.com.cn>
2021-09-24 11:35:51 +0800

    reset code

--- src/views/partsrfq/editordetail/components/rfqDetailTpzs/components/quotationScoringHz/components/tableListSupplier.vue
+++ src/views/partsrfq/editordetail/components/rfqDetailTpzs/components/quotationScoringHz/components/tableListSupplier.vue
@@ -1,18 +1,15 @@
 <!--
  * @Author: yuszhou
  * @Date: 2021-05-28 15:03:47
- * @LastEditTime: 2021-09-24 11:33:52
+ * @LastEditTime: 2021-09-24 11:35:33
  * @LastEditors: Please set LastEditors
  * @Description: 特殊表格实现,如果fixed模块需要改动，需要将里面部分提为组件。
  * @FilePath: \front-web\src\views\partsrfq\editordetail\components\rfqDetailTpzs\components\quotationScoringHz\components\table.vue
 -->
-<template>  
-  <div class="fatherTable">
-  <div class="table-fiexed-left" id="tableLeft"></div>
-  <div class="table-fiexed-right" id="tableRight"></div>
-  <div   class="selsTable" id="elTable" :style="{paddingTop:paddingTop}">
+<template>
+<div class="supplier">
+  <div class="selsTable" :style="{paddingTop:paddingTop}">
     <el-table
-    
       tooltip-effect="light"
       :height="height"
       :data="tableData"
@@ -21,7 +18,7 @@
       :empty-text="$t('LK_ZANWUSHUJU')"
       ref='table'
     >
-      <template v-for='(item,index) in tableTitle' >
+      <template v-for='(item,index) in tableTitle'>
         <!-----------------表格中内容模块------------------------>
         <el-table-column
           :key="index"
@@ -296,13 +293,6 @@ export default{
     return {
     supplierTableTop:supplierTableTop
   }},
-  watch:{
-    tableData(){
-        setTimeout(() => {
-           this.getTopWidth() 
-        }, 2000);
-    }
-  },
   methods:{
     getPorpsNumber(props){return getPorpsNumber(props)},
     optionPage(items,index){
@@ -338,6 +328,7 @@ export default{
       }
     },
     cellClassName({row, column, rowIndex, columnIndex}) {
+      console.log(column);
       if(column.label =='LC A Price' ) {
         return 'priceUnderLinePrice'
       }      
@@ -417,10 +408,7 @@ export default{
       float: left;
     }
   }
-
-  .el-table{
-        // position: absolute;
-// top: 0px;
+  .el-table {
     overflow: visible;
     ::v-deep.cell{
       overflow: visible;
@@ -512,15 +500,7 @@ export default{
   }
   .selsTable{
     width: 100%;
-  }
-  .fatherTable{
     overflow-x: scroll;
-    position: relative;
   }
-  .table-fiexed-left{
-    position:absolute;
-    height: 100%;
-    width:100px;
-    left: 0;
-  }
+
 </style>
