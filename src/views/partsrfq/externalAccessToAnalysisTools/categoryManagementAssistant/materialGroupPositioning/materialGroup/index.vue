60a7268db18285f85b30fa02c57aea729d705944
舒杰 <jieshu@deloitte.com.cn>
2021-09-27 16:10:53 +0800

    fix bug

--- src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/materialGroupPositioning/materialGroup/index.vue
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/materialGroupPositioning/materialGroup/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 舒杰
  * @Date: 2021-08-02 10:13:24
- * @LastEditTime: 2021-09-27 16:06:17
+ * @LastEditTime: 2021-09-27 16:10:06
  * @LastEditors: 舒杰
  * @Description: 材料组定位
  * @FilePath: \front-sourcing\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\materialGroupPositioning\materialGroup\index.vue
@@ -36,7 +36,7 @@
       ></piecewise>
     </div>
     <!-- 业务影响度特征分布情况 供应复杂度特征分布 -->
-    <el-row gutter="50" class="margin-top35">
+    <el-row gutter="50" class="margin-top45">
       <el-col :span="8">
         <div class="cardTitle flex-align-center">
           <icon
