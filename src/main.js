6de256fe5a4db1c6c2d9853e9684683a4605c7b7
yuan <1132524215@qq.com>
2021-09-27 10:22:16 +0800

    整合自定义指令

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /*
  * @Author: yuszhou
  * @Date: 2021-02-19 14:29:06
- * @LastEditTime: 2021-09-24 14:56:16
- * @LastEditors: zbin
+ * @LastEditTime: 2021-09-27 10:21:53
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \rise\src\main.js
  */
@@ -15,12 +15,9 @@ import '@/assets/style/global/index.scss'
 import './utils/mydirect'
 import './permission'
 
-import Directives from '@/utils/directives/index'
 require('./auto').init()
 Vue.config.productionTip = false
 Vue.prototype.$t = (key, value) => i18n.t(key, value);
-Vue.prototype.$EventBus = new Vue()
-Vue.use(Directives)
 Promise.all([
     import('rise/web/lang/zh.js'),
     import('rise/web/lang/en.js')
