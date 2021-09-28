5b80b67982a56dd6922a082398033c7e712312ee
舒杰 <jieshu@deloitte.com.cn>
2021-09-28 18:07:05 +0800

    fix bug

--- src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/costAnalysisMain/components/data.js
+++ src/views/partsrfq/externalAccessToAnalysisTools/categoryManagementAssistant/internalDemandAnalysis/costAnalysisMain/components/data.js
@@ -1,7 +1,7 @@
 /*
  * @Author: youy
  * @Date: 2021-08-02 15:46:31
- * @LastEditTime: 2021-09-28 16:28:43
+ * @LastEditTime: 2021-09-28 17:55:15
  * @LastEditors: 舒杰
  * @Description: In User Settings Edit
  * @FilePath: \front-sourcing\src\views\partsrfq\externalAccessToAnalysisTools\categoryManagementAssistant\internalDemandAnalysis\costAnalysisMain\components\data.js
@@ -9,9 +9,9 @@
 
 export const tableTitle = [
   {props: 'partNum', name: '零件号', key: 'LINGJIANHAO',width:120,tooltip: true},
-  {props: 'fsNum', name: 'FS号', key: 'FSHAO',width:120,tooltip: true},
+  {props: 'fsNum', name: 'FS号', key: 'FSHAO',width:140,tooltip: true},
   {props: 'supplierName', name: '供应商', key: 'GONGYINGSHANG',tooltip: true},
-  {props: 'linie', name: 'Linie', key: 'LINIEXIAOXIE',tooltip: true},
+  {props: 'linie', name: 'Linie', key: 'LINIEXIAOXIE',width:80,tooltip: true},
   {props: 'nomiDate', name: '定点日期', key: 'DINGDIANRIQI',tooltip: true},
   {props: 'cartypeProjectZh', name: '车型项目', key: 'CHEXINGXIANGMU',tooltip: true},
 ]
