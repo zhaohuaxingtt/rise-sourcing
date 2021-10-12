b5e5714191f9efb357f1014b1ed9dbcd385a1122
yujipeng <917955345@qq.com>
2021-10-12 21:34:35 +0800

    提交CBD汇总页面

--- src/api/aeko/approve/index.js
+++ src/api/aeko/approve/index.js
@@ -1,8 +1,8 @@
 /*
  * @Autor: Hao,Jiang
  * @Date: 2021-09-27 11:38:20
- * @LastEditors: Hao,Jiang
- * @LastEditTime: 2021-09-28 13:52:51
+ * @LastEditors: YoHo
+ * @LastEditTime: 2021-10-12 19:46:38
  * @Description: aeko 审批
  */
 import axios from '@/utils/axios'
@@ -39,20 +39,43 @@ export function getRoleUserList(params) {
 }
 
 //查询AKEO已审批列表
-export function queryApproved(params){
+export function queryApproved(params) {
   return requst({
     url: '/aeko/approvedPage',
     method: 'POST',
-    data:params
+    data: params
   })
 }
 
 //查询AKEO待审批列表
-export function pendingApprovalList(params){
+export function pendingApprovalList(params) {
   return requst({
-        url: '/aeko/approve/distribution/approvalPendingPage',
-        method: 'POST',
-        data:params
-      }
+    url: '/aeko/approve/distribution/approvalPendingPage',
+    method: 'POST',
+    data: params
+  }
   )
+}
+
+// 查询CBD汇总表/aeko/get/alterationCbdSummarySnapshot/{workFlowId}
+
+export function alterationCbdSummary({ workFlowId }) {
+  return requst({
+    url: `/aeko/get/alterationCbdSummary/${workFlowId}`,
+    method: 'GET',
+  })
+}
+// 切换零件下拉框接口
+export function getSwitchParts({workFlowId}){
+  return requst({
+    url: `/aeko/purchasing/aekopart/getSwitchParts/${workFlowId}`,
+    method: 'GET',
+  })
+}
+// 查询CBD汇总表其余数据
+export function cbdDataQuery({ workFlowId, quotationId }) {
+  return requst({
+    url: `/aeko/get/cbdDataQuery/${workFlowId}/${quotationId}`,
+    method: 'GET',
+  })
 }
