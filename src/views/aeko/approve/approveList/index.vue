cfeb282e495f6f9dd95b6a8a4db4f23385122bb7
姜浩 <543333675@qq.com>
2021-10-28 17:45:51 +0800

    AEKO 分配主岗位postId

--- src/views/aeko/approve/approveList/index.vue
+++ src/views/aeko/approve/approveList/index.vue
@@ -2,7 +2,7 @@
  * @Autor: Hao,Jiang
  * @Date: 2021-09-23 15:32:13
  * @LastEditors: Hao,Jiang
- * @LastEditTime: 2021-10-28 15:33:30
+ * @LastEditTime: 2021-10-28 17:45:15
  * @Description: 
 -->
 <template>
@@ -245,7 +245,8 @@ export default {
               presetChiefIds.forEach((p, pindex) => {
                 preSetChief.push({
                   code: String(p),
-                  value: presetChiefNames[pindex]
+                  value: presetChiefNames[pindex],
+                  postId: o.postId || ''
                 })
               })
               o.chiefsOptions = preSetChief
@@ -316,6 +317,7 @@ export default {
             return {
               value: this.$i18n.locale === "zh" ? item.nameZh : item.nameEn,
               code: String(item.id),
+              postId: item.positionDTO && item.positionDTO.id || ''
             }
           })
           const chiefsOptions = row.chiefsOptions || []
@@ -350,15 +352,15 @@ export default {
       let parmas = selectedData.map(o => {
         const choseChiefs = o.chiefNames || []
         const chiefName = choseChiefs.map(chiefId => {
-          const cName = (o.selectOptions || []).find(buyer => buyer.code === chiefId) || {}
+          const chiefs = (o.selectOptions || []).find(buyer => buyer.code === chiefId) || {}
           console.log('selectOptions', o.selectOptions || [])
           return {
             id: o.id || '',
-            postId: o.postId || '',
+            postId: chiefs.postId || '',
             auditType: o.auditType || '',
             chiefId,
             aekoManageId: o.aekoManageId || '',
-            chiefName: cName.value || '',
+            chiefName: chiefs.value || '',
           }
         })
         return chiefName
