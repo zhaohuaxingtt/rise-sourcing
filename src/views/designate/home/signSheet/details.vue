329857b316136ec3bc00dbd1945ab1ba640fa7c4
yuan <1132524215@qq.com>
2021-11-09 17:36:13 +0800

    M签字单 MTZ定点申请单

--- src/views/designate/home/signSheet/details.vue
+++ src/views/designate/home/signSheet/details.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: Haojiang
  * @Date: 2021-06-24 17:53:08
- * @LastEditTime: 2021-11-08 17:57:15
- * @LastEditors:  
+ * @LastEditTime: 2021-11-09 11:03:46
+ * @LastEditors: Please set LastEditors
  * @Description: m签字单新增、详情
  * @FilePath: /front-web/src/views/designate/home/signSheet/newSignSheet.vue
 -->
