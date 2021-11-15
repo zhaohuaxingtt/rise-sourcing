0b3c0099caa458eea758e7a2f27d5c231f33b9ad
余继鹏 <917955345@qq.com>
2021-11-15 15:33:05 +0800

    调整终止费UI

--- src/views/aeko/approve/approveDetails/components/damages.vue
+++ src/views/aeko/approve/approveDetails/components/damages.vue
@@ -7,112 +7,115 @@
 <template>
   <!-- 终止费 -->
   <iCard class="damages">
-      <template #header>
-        <div class="header">
-          <div>
-            <span class="title">{{ language('LK_DAMAGES_ZHONGZHIFEI','终⽌费') }}</span>
-          </div>
+    <template #header>
+      <div class="header">
+        <div>
+          <span class="title">{{
+            language("LK_DAMAGES_ZHONGZHIFEI", "终⽌费")
+          }}</span>
         </div>
-      </template>
-      <div class="contain">
-        <span class="contain-label">{{ language('LK_DAMAGES_ZHONGZHIFEI','终⽌费') }}:</span>
-        <iInput class="contain-input" disabled v-model.trim="value"></iInput>
       </div>
+    </template>
+    <iFormGroup class="subCost" inline>
+      <iFormItem :label="language('LK_DAMAGES_ZHONGZHIFEI', '终⽌费')">
+        <iText>{{ value }}</iText>
+      </iFormItem>
+    </iFormGroup>
   </iCard>
 </template>
 
 <script>
-import {
-  iCard,
-  iInput,
-  iMessage,
-} from 'rise'
+import { iCard, iInput, iMessage, iFormGroup, iFormItem, iText } from "rise";
 import { getTerminationPrice } from "@/api/aeko/approve";
 import { floatFixNum } from "../data.js";
 export default {
-  components:{
+  components: {
     iCard,
     iInput,
+    iFormGroup,
+    iFormItem,
+    iText,
   },
-  props:{
-    basicInfo:{
-      type:Object,
-      default:()=>{},
+  props: {
+    basicInfo: {
+      type: Object,
+      default: () => {},
     },
     disabled: {
       type: Boolean,
-      default: false
+      default: false,
     },
-    workFlowId:{
-      type:String,
-      default:''
+    workFlowId: {
+      type: String,
+      default: "",
     },
-    quotationId:{
-      type:String,
-      default:'',
-    }
+    quotationId: {
+      type: String,
+      default: "",
+    },
   },
-  data(){
-    return{
-      value:'',
-      btnLoading:false,
-    }
+  data() {
+    return {
+      value: "",
+      btnLoading: false,
+    };
   },
-  methods:{
-    init(){
-      this.workFlowId?this.getTerminationPrice():this.getTerminationPriceByLinie();
+  methods: {
+    init() {
+      this.workFlowId
+        ? this.getTerminationPrice()
+        : this.getTerminationPriceByLinie();
     },
     // 获取终止费
-    async getTerminationPrice(){
-      const {workFlowId,quotationId} = this;
+    async getTerminationPrice() {
+      const { workFlowId, quotationId } = this;
       await getTerminationPrice({
         workFlowId,
         quotationId,
-      }).then((res)=>{
-        if(res.code == 200){
-          this.value = floatFixNum(res.data) || '';
-        }else{
-          iMessage.error(this.$i18n.locale === "zh" ? res.desZh : res.desEn)
+      }).then((res) => {
+        if (res.code == 200) {
+          this.value = floatFixNum(res.data) || "";
+        } else {
+          iMessage.error(this.$i18n.locale === "zh" ? res.desZh : res.desEn);
         }
-      })
+      });
     },
-    getTerminationPriceByLinie(){
-      const {basicInfo={}} = this; 
-      const {quotationPriceSummaryInfo={}} = basicInfo;
+    getTerminationPriceByLinie() {
+      const { basicInfo = {} } = this;
+      const { quotationPriceSummaryInfo = {} } = basicInfo;
       const terminationPrice = quotationPriceSummaryInfo.terminationPrice;
       this.value = floatFixNum(terminationPrice);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .damages{
-    .header {
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
+.damages {
+  .header {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-      .title {
-        height: 25px;
-        line-height: 25px;
-        font-size: 18px;
-        font-weight: bold;
-        color: #131523;
-      }
+    .title {
+      height: 25px;
+      line-height: 25px;
+      font-size: 18px;
+      font-weight: bold;
+      color: #131523;
     }
-    .contain{
-      display: flex;
-      align-items: center;
-      .contain-label{
-        flex-shrink: 0;
-        margin-right: 30px;
-      }
-      .contain-input{
-        width: 300px;
-      }
-    }
-
   }
+
+  .subCost {
+    ::v-deep .el-form-item {
+      margin-bottom: 0;
+
+      .el-form-item__label {
+        width: 120px;
+        font-size: 16px;
+      }
+    }
+  }
+}
 </style>
