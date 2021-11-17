6dcc5425ee4247ba07b2668f10e87526e43e1e4d
Luo <rime_ls@qq.com>
2021-11-17 14:52:00 +0800

    fix bug

--- src/views/project/progressmonitoring/monitorDetail/components/delayReson/data.js
+++ src/views/project/progressmonitoring/monitorDetail/components/delayReson/data.js
@@ -2,9 +2,9 @@
  * @Author: Luoshuang
  * @Date: 2021-09-24 13:54:07
  * @LastEditors: Luoshuang
- * @LastEditTime: 2021-11-03 17:33:11
+ * @LastEditTime: 2021-11-17 14:43:57
  * @Description: 
- * @FilePath: \front-web\src\views\project\progressmonitoring\monitorDetail\components\delayReson\data.js
+ * @FilePath: \front-sourcing\src\views\project\progressmonitoring\monitorDetail\components\delayReson\data.js
  */
 
 export const tableTitle = [
@@ -12,6 +12,7 @@ export const tableTitle = [
   {props:'projectPurchaser',name:'项目采购员', key: "XIANGMUCAIGOUYUAN", tooltip: true}, 
   {props:'partNum',name:'零件号', key: "LINGJIANHAO", tooltip: true}, 
   {props:'partName',name:'零件中文名称', key: "LINGJIANZHONGWENMINGCHENG", tooltip: true}, 
+  {props:'partNameDe',name:'零件德文名称', key: "LINGJIANDEWENMINGCHENG", tooltip: true},
   {props:'isBmg',name:'是否BMG', key: "SHIFOUBMG", tooltip: true}, 
   {props:'partPeriodDesc',name:'零件状态', key: "LINGJIANZHUANGTAI", tooltip: true}, 
   {props:'planDate',name:'原计划时间', key: "YUANJIHUASHIJIAN", tooltip: true}, 
