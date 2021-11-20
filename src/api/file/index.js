abe7f25795db2aceb0e969ef80aee7ee419a54ae
ArthurLyu <artlyu@deloitte.com.cn>
2021-11-20 21:40:03 +0800

    fileApi配置处理1

--- src/api/file/index.js
+++ src/api/file/index.js
@@ -10,7 +10,7 @@ import axios from '@/utils/axios.download'
 import { serialize } from '@/utils'
 import httpAxios from '@/utils/axios'
 const requst = axios(process.env.VUE_APP_COMMON)
-const fileRequst = axios(process.env.VUE_APP_NEW_COMMON)
+const fileRequst = axios(process.env.VUE_APP_BASE_UPLOAD_API)
 const sourcing = httpAxios(process.env.VUE_APP_SOURCING)
 export function downloadFile(parmars) {
     return requst({
@@ -21,15 +21,15 @@ export function downloadFile(parmars) {
 
 export function downloadUdFile(params) {
     return fileRequst({
-        url: `/fileudApi/udDown?${ Array.isArray(params) ? serialize(params.map(id => ({ fileIds: id })), Array) : 'fileIds=' + params }`,
+        url: `/udDown?${ Array.isArray(params) ? serialize(params.map(id => ({ fileIds: id })), Array) : 'fileIds=' + params }`,
         method: 'POST'
     })
 }
 
 // 自定义文件名下载
-export function downloadUdFileWithName(params,fileName) {
+export function downloadUdFileWithName(params, fileName) {
     return fileRequst({
-        url: `/fileudApi/udDownWithName?${ Array.isArray(params) ? serialize(params.map(id => ({ fileIds: id })), Array) : 'fileIds=' + params }&fileName=${fileName}`,
+        url: `/udDownWithName?${ Array.isArray(params) ? serialize(params.map(id => ({ fileIds: id })), Array) : 'fileIds=' + params }&fileName=${fileName}`,
         method: 'POST'
     })
 }
@@ -37,7 +37,7 @@ export function downloadUdFileWithName(params,fileName) {
 
 //sourcing 获取文件列表不需要分页。
 export function getFile(params) {
-    let {fileType,hostId,isAsc=true,sortColumn='uploadDate'} = params
+    let { fileType, hostId, isAsc = true, sortColumn = 'uploadDate' } = params
     return sourcing({
         url: `/file-histories/file-history/${hostId}/${sortColumn}/${isAsc}/${fileType}`,
         method: 'get'
@@ -45,7 +45,7 @@ export function getFile(params) {
 }
 //sourcing 获取文件列表带分页。
 export function getFiles(params) {
-    let {fileType,hostId,isAsc=true,sortColumn='uploadDate',page,currPage} = params
+    let { fileType, hostId, isAsc = true, sortColumn = 'uploadDate', page, currPage } = params
     return sourcing({
         url: `/file-histories/page/${hostId}/${sortColumn}/${isAsc}/${fileType}/${page}/${currPage}`,
         method: 'get'
@@ -56,7 +56,7 @@ export function deleteFiles(params) {
     return sourcing({
         url: `/file-histories/delete-files`,
         method: 'DELETE',
-        data:params
+        data: params
     })
 }
 
