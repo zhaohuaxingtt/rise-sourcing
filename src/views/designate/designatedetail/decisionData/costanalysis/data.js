9dbf5cddb0737f31b3b8c4f4654c48be03f993df
周玉松 <yuszhou@deloitte.com.cn>
2021-11-25 15:37:32 +0800

    fix bug

--- src/views/designate/designatedetail/decisionData/costanalysis/data.js
+++ src/views/designate/designatedetail/decisionData/costanalysis/data.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-02 19:43:59
- * @LastEditTime: 2021-11-06 11:26:07
+ * @LastEditTime: 2021-11-25 10:57:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \front-web\src\views\designate\designatedetail\decisionData\costanalysis\data.js
@@ -18,7 +18,7 @@ export const arrayOfselect = [
 
 export const tableTitle = [
   {props:'analysisName',name:'分析名称',key: 'LK_FENXIMINGC',tooltip:true},
-  {props:'stuffName',name:'材料组',key: 'LK_CAILIAOZUBIANHAO',tooltip:true},
+  {props:'stuffName',name:'材料组',key: 'LK_CAILIAOZUBIANHAOALLSY',tooltip:true},
   {props:'rfqId',name:'RFQ号',key: 'LK_RFQHAO',tooltip:true},
   {props:'operate',name:'操作',key: 'LK_CAOZUO',tooltip:true},
   {props:'flag',name:'是否展示',key: 'LK_SHIFOUZHANSHI',tooltip:true},
