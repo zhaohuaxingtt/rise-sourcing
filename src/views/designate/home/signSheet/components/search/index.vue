12bfa54a57dab9885f091fcd5ac1db24be443f38
Luo <rime_ls@qq.com>
2021-12-01 16:53:57 +0800

    定点管理权限

--- src/views/designate/home/signSheet/components/search/index.vue
+++ src/views/designate/home/signSheet/components/search/index.vue
@@ -1,10 +1,10 @@
 <!--
  * @Author: Haojiang
  * @Date: 2021-06-28 09:29:08
- * @LastEditTime: 2021-11-02 20:42:10
- * @LastEditors:  
+ * @LastEditTime: 2021-12-01 14:44:35
+ * @LastEditors: Luoshuang
  * @Description: m签字单搜索
- * @FilePath: /front-web/src/views/designate/home/signSheet/components/search/index.vue
+ * @FilePath: \front-sourcing\src\views\designate\home\signSheet\components\search\index.vue
 -->
 
 <template>
@@ -17,42 +17,37 @@
   >
     <el-form>
       <!-- 申请单号 -->
-      <el-form-item :label="language('nominationLanguage_ShenQingDanHao','申请单号')">
+      <el-form-item :label="language('nominationLanguage_ShenQingDanHao','申请单号')" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_NOMINATEID|申请单号">
         <iInput
           v-model="form.nominateId"
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_NOMINATEID|申请单号"
           :placeholder="language('LK_QINGSHURU','请输入')"
         ></iInput>
       </el-form-item>
       <!-- 零件号 -->
-      <el-form-item :label="language('nominationLanguage_LingJianHao', '零件号')">
+      <el-form-item :label="language('nominationLanguage_LingJianHao', '零件号')" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_PARTNUM|零件号">
         <iInput
           v-model="form.partNum"
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_PARTNUM|零件号"
           :placeholder="language('LK_QINGSHURU','请输入')"
         ></iInput>
       </el-form-item>
       <!-- 询价采购员 -->
-      <el-form-item :label="language('CSF','CSF')">
+      <el-form-item :label="language('CSF','CSF')" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_BUYERNAME|CSF">
         <iInput
           v-model="form.buyerName"
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_BUYERNAME|CSF"
           :placeholder="language('LK_QINGSHURU','请输入')"
         ></iInput>
       </el-form-item>
       <!-- LINIE -->
-      <el-form-item label="LINIE">
+      <el-form-item label="LINIE" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_LINIENAME|linieName">
         <iInput
           v-model="form.linieName"
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_LINIENAME|linieName"
           :placeholder="language('LK_QINGSHURU','请输入')"
         ></iInput>
       </el-form-item>
       <!-- 签字单状态 -->
-      <el-form-item :label="language('QIANZIDANZHUANGTAI','签字单状态')">
+      <el-form-item :label="language('QIANZIDANZHUANGTAI','签字单状态')" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_STATUS|签字单状态">
         <iSelect
           v-model="form.status"
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_STATUS|签字单状态"
           :placeholder="language('LK_QINGXUANZE','请选择')"
         >
           <el-option
@@ -68,10 +63,9 @@
         </iSelect>
       </el-form-item>
       <!-- 复核是否截至 -->
-      <el-form-item :label="language('FUHESHIFOUJIEZHI','复核是否截至')">
+      <el-form-item :label="language('FUHESHIFOUJIEZHI','复核是否截至')" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_ISPASSCHECK|复核是否截至">
         <iSelect
           v-model="form.isPassCheck"
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_ISPASSCHECK|复核是否截至"
           :placeholder="language('LK_QINGXUANZE','请选择')"
         >
           <el-option
@@ -85,27 +79,24 @@
         </iSelect>
       </el-form-item>
       <!-- 会议名称 -->
-      <el-form-item :label="language('HUIYIMINGCHENG','会议名称')">
+      <el-form-item :label="language('HUIYIMINGCHENG','会议名称')" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_METTINGNAME|会议名称">
         <iInput
           v-model="form.meetingName"
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_METTINGNAME|会议名称"
           :placeholder="language('LK_QINGSHURU','请输入')"
         ></iInput>
       </el-form-item>
       <!-- 复核开始日期 -->
-      <el-form-item :label="language('JIEZHIQIZHIRIQI','截止起止日期')">
+      <el-form-item :label="language('JIEZHIQIZHIRIQI','截止起止日期')" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_CHECKDATE|截止起止日期">
         <iDatePicker
           v-model='form.checkDate'
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_CHECKDATE|截止起止日期"
           type="daterange"
           value-format="yyyy-MM-dd HH:mm:ss">
         </iDatePicker>
       </el-form-item>
       <!-- 签字单号 -->
-      <el-form-item :label="language('QIANZIDANHAO','签字单号')">
+      <el-form-item :label="language('QIANZIDANHAO','签字单号')" v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_SIGNID|签字单号">
         <iInput
           v-model="form.signCode"
-          v-permission.auto="SOURCING_NOMINATION_SIGNSHEET_SIGNID|签字单号"
           :placeholder="language('LK_QINGSHURU','请输入')"
         ></iInput>
       </el-form-item>
