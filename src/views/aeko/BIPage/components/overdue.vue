96d372bd497228c3dd954ca27eee77b2acb191da
余继鹏 <917955345@qq.com>
2021-12-17 10:32:46 +0800

    修改权限key值

--- src/views/aeko/BIPage/components/overdue.vue
+++ src/views/aeko/BIPage/components/overdue.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="page-content" v-permission.auto="REPORTMGMT_STATUSREPORT_PROCESS_PBI|报表管理-状态跟踪报表">
+	<div class="page-content" v-permission.auto="AEKO_MANAGELIST_BUTTON_YUQIBIBAOBIAO|逾期BI报表">
 		<iCard id='powerBiReport'>
 			
 		</iCard>
