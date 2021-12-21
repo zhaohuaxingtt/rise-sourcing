016a676b5f4ca97351d35e4c6398d7ce945665f2
Luo <rime_ls@qq.com>
2021-12-21 18:09:40 +0800

    update

--- src/views/project/schedulingassistant/part/components/selectKw/index.vue
+++ src/views/project/schedulingassistant/part/components/selectKw/index.vue
@@ -2,7 +2,7 @@
  * @Author: Luoshuang
  * @Date: 2021-10-20 13:55:59
  * @LastEditors: Luoshuang
- * @LastEditTime: 2021-11-17 22:13:56
+ * @LastEditTime: 2021-12-21 17:14:27
  * @Description: 
  * @FilePath: \front-sourcing\src\views\project\schedulingassistant\part\components\selectKw\index.vue
 -->
@@ -48,6 +48,8 @@ export default {
     dialogVisible(val) {
       if (val) {
         this.valueTemp = this.value || ''
+      } else {
+        this.valueTemp = ''
       }
     }
   },
