86056f08f4bd58568cf762bc5ca3e6e5b88d2b15
ArthurLyu <artlyu@deloitte.com.cn>
2021-12-31 11:54:28 +0800

    【模具投资】预算管理，生产工厂取值错误

--- src/api/ws2/baApply/index.js
+++ src/api/ws2/baApply/index.js
@@ -8,67 +8,76 @@
  */
 import axios from '@/utils/axios';
 
-const VUE_APP_APPLY = axios(process.env.VUE_APP_TOOLING  + '/baPartsApply');
+const VUE_APP_APPLY = axios(process.env.VUE_APP_TOOLING + '/baPartsApply');
+const VUE_APP_CARTYPE = axios(process.env.VUE_APP_TOOLING + '/cartype')
 
 //  ba申请-车型列表
 export function findCarTypeList(parmars) {
-  return VUE_APP_APPLY({
-      url: '/findCarTypeList',
-      method: 'POST',
-      data: parmars
-  })
+    return VUE_APP_APPLY({
+        url: '/findCarTypeList',
+        method: 'POST',
+        data: parmars
+    })
 }
 
 //  采购工厂列表
 export function getPurchaseFactoryPullDown(parmars) {
-  return VUE_APP_APPLY({
-      url: '/getPurchaseFactoryPullDown',
-      method: 'GET',
-      data: parmars
-  })
+    return VUE_APP_APPLY({
+        url: '/getProductionFactoryPullDown',
+        method: 'GET',
+        data: parmars
+    })
 }
 
 //  模具预算状态下拉
 export function getBudgetStatusPullDown(parmars) {
-  return VUE_APP_APPLY({
-      url: '/getBudgetStatusPullDown',
-      method: 'GET',
-      data: parmars
-  })
+    return VUE_APP_APPLY({
+        url: '/getBudgetStatusPullDown',
+        method: 'GET',
+        data: parmars
+    })
 }
 
 //  ba申请-车型列表
 export function findBaPartsList(parmars) {
-  return VUE_APP_APPLY({
-      url: '/findBaPartsList',
-      method: 'POST',
-      data: parmars
-  })
+    return VUE_APP_APPLY({
+        url: '/findBaPartsList',
+        method: 'POST',
+        data: parmars
+    })
 }
 
 //  模具预算状态下拉
 export function getBaAccountType(parmars) {
-  return VUE_APP_APPLY({
-      url: '/getBaAccountType',
-      method: 'GET',
-      data: parmars
-  })
+    return VUE_APP_APPLY({
+        url: '/getBaAccountType',
+        method: 'GET',
+        data: parmars
+    })
 }
 
 //  BA账户类型
 export function getBaAccountTypePullDown(parmars) {
-  return VUE_APP_APPLY({
-      url: '/getBaAccountTypePullDown',
-      method: 'GET',
-      data: parmars
-  })
+    return VUE_APP_APPLY({
+        url: '/getBaAccountTypePullDown',
+        method: 'GET',
+        data: parmars
+    })
 }
 
 //  获取车型列表下拉
 export function getBaCarPullDown(parmars) {
-  return VUE_APP_APPLY({
-      url: '/getBaCarPullDown',
-      method: 'GET',
-      data: parmars
-  })
+    return VUE_APP_APPLY({
+        url: '/getBaCarPullDown',
+        method: 'GET',
+        data: parmars
+    })
+}
+
+// 获取生产工厂下拉列表
+export function getProductionFactoryPullDown() {
+    return VUE_APP_CARTYPE({
+        url: '/getProductionFactoryPullDown',
+        method: 'GET',
+    })
 }
