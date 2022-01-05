c30f92f2ec2aac6a39f56db72b18d270d71b68d2
曹鹏 <1076613617@qq.com>
2022-01-05 11:26:39 +0800

    合并

--- src/api/categoryManagementAssistant/internalDemandAnalysis/output.js
+++ src/api/categoryManagementAssistant/internalDemandAnalysis/output.js
@@ -1,15 +1,15 @@
 /*
  * @Author: 舒杰
  * @Date: 2021-08-05 19:04:35
- * @LastEditTime: 2022-01-05 10:08:11
- * @LastEditors: caopeng
+ * @LastEditTime: 2021-08-05 19:05:27
+ * @LastEditors: 舒杰
  * @Description: 产量总览
- * @FilePath: \front-sourcing-new\src\api\categoryManagementAssistant\internalDemandAnalysis\output.js
+ * @FilePath: \front-sourcing\src\api\categoryManagementAssistant\internalDemandAnalysis\output.js
  */
 import axios from '@/utils/axios';
 
 const request = axios(process.env.VUE_APP_POWEBI_KPI + '/web/aon');
-const requstApi = axios(process.env.VUE_APP_POWEBI_KPI + '/api/aon')
+
 //获取品类管理-产量总览pbi的信息
 
 export function getCmOutputPbi() {
@@ -18,10 +18,3 @@ export function getCmOutputPbi() {
       method: 'GET'
     });
 }
-//重置接口
-export function listQuantity(parmars) {
-    return requstApi({
-        url: '/categoryReport/listQuantity?categoryCode='+parmars,
-        method: 'GET'
-      });
-  }
--- src/api/partsrfq/purchaseAmountOverall/index.js
+++ src/api/partsrfq/purchaseAmountOverall/index.js
@@ -2,13 +2,12 @@
  * @version: 1.0
  * @Author: zbin
  * @Date: 2021-08-05 20:49:33
- * @LastEditors: caopeng
+ * @LastEditors: zbin
  * @Descripttion: your project
  */
 import axios from '@/utils/axios'
 
 const requst = axios(process.env.VUE_APP_POWEBI_KPI + '/web/aon')
-const requstApi = axios(process.env.VUE_APP_POWEBI_KPI + '/api/aon')
 
 // pbi-采购金额总览
 export function getPurchaseAmountPbi(parmars) {
@@ -18,10 +17,3 @@ export function getPurchaseAmountPbi(parmars) {
         data: parmars
     })
 }
-//重置接口
-export function listAmount(parmars) {
-    return requstApi({
-        url: '/categoryReport/listAmount?categoryCode='+parmars,
-        method: 'GET'
-      });
-  }
