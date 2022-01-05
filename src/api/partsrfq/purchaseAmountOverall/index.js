dd604da344a2284dac384090e54b0b00d289dbbf
曹鹏 <1076613617@qq.com>
2022-01-05 11:10:12 +0800

    合并

--- src/api/partsrfq/purchaseAmountOverall/index.js
+++ src/api/partsrfq/purchaseAmountOverall/index.js
@@ -2,12 +2,13 @@
  * @version: 1.0
  * @Author: zbin
  * @Date: 2021-08-05 20:49:33
- * @LastEditors: zbin
+ * @LastEditors: caopeng
  * @Descripttion: your project
  */
 import axios from '@/utils/axios'
 
 const requst = axios(process.env.VUE_APP_POWEBI_KPI + '/web/aon')
+const requstApi = axios(process.env.VUE_APP_POWEBI_KPI + '/api/aon')
 
 // pbi-采购金额总览
 export function getPurchaseAmountPbi(parmars) {
@@ -17,3 +18,10 @@ export function getPurchaseAmountPbi(parmars) {
         data: parmars
     })
 }
+//重置接口
+export function listAmount(parmars) {
+    return requstApi({
+        url: '/categoryReport/listAmount?categoryCode='+parmars,
+        method: 'GET'
+      });
+  }
