31928442ebae952f4518394950592962c338d8c5
曹鹏 <1076613617@qq.com>
2022-01-12 16:43:25 +0800

    谈判助手导出取消自动下载

--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -232,7 +232,7 @@ export const downloadPdfMixins = {
                                 downloadPDF({
                                     idEle: '#card' + key,
                                     pdfName: name,
-                                    exportPdf: true,
+                                    exportPdf: false,
                                     waterMark: true,
                                     callback: async (pdf, pdfName) => {
                                         try {
