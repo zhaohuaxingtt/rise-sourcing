edc69ac62a14977b51bbab3377a246e2ee832862
wentliao <wentliao@deloitte.com.cn>
2022-01-29 00:05:26 +0800

    update rfq详情 询价图纸文字的展示逻辑

--- src/views/partsrfq/editordetail/components/rfqDetailInfo/components/inquiryAttachment/index.vue
+++ src/views/partsrfq/editordetail/components/rfqDetailInfo/components/inquiryAttachment/index.vue
@@ -1,15 +1,15 @@
 <!--
  * @Author: your name
  * @Date: 2021-11-08 11:48:04
- * @LastEditTime: 2022-01-07 10:00:13
- * @LastEditors: YoHo
+ * @LastEditTime: 2022-01-28 23:54:21
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \front-sourcing\src\views\partsrfq\editordetail\components\rfqDetailInfo\components\inquiryAttachment\index.vue
 -->
 <template>
   <div>
     <inquiry-attachment v-if='rfqId' :rfqId='rfqId'/>
-    <inquiry-drawing v-if='rfqId' :rfqId='rfqId' class="margin-top20"/>
+    <inquiry-drawing :baseInfo="baseInfo" v-if='rfqId' :rfqId='rfqId' class="margin-top20"/>
   </div>
 </template>
 
@@ -20,6 +20,10 @@ import inquiryDrawing from './components/inquiryDrawing'
 export default {
   props:{
     rfqId:String,
+    baseInfo:{
+      type:Object,
+      default:()=>{},
+    }
   },
   components: {
     inquiryAttachment,
