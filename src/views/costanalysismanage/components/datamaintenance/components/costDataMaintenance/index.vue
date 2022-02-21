238c2e33456646464253fc4cc406f1efab3317cb
tyrliu <tyrliu@deloitte.com.cn>
2022-02-21 11:41:59 +0800

    pdf打开

--- src/views/costanalysismanage/components/datamaintenance/components/costDataMaintenance/index.vue
+++ src/views/costanalysismanage/components/datamaintenance/components/costDataMaintenance/index.vue
@@ -195,7 +195,8 @@ export default {
       //   applicationName: "rise",
       //   fileList: row.fileName
       // })
-      downloadUdFile(row.uploadId)
+      // downloadUdFile(row.uploadId)
+      window.open(row.filePath,'_blank')
     },
     // 删除
     handleDelete() {
