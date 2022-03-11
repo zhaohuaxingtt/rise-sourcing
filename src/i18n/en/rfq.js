cffef72303665bbcc7051f5de7588414f327c6e5
余继鹏 <917955345@qq.com>
2022-03-11 17:10:45 +0800

    fix bug CRW-6826

---
+++ src/i18n/en/rfq.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: YoHo
+ * @Date: 2022-03-11 16:51:30
+ * @LastEditTime: 2022-03-11 16:54:48
+ * @LastEditors: YoHo
+ * @Description: rfq管理
+ */
+ export default {
+  XINSHENGQINGMUBIAOJIA_QIWANGMUBIAOJIABUNENGWEIKONG : 'The expected target price should not be blank'
+}
---
+++ src/i18n/zh/rfq.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: YoHo
+ * @Date: 2022-03-11 16:51:30
+ * @LastEditTime: 2022-03-11 16:51:30
+ * @LastEditors: YoHo
+ * @Description: rfq管理
+ */
+ export default {
+  XINSHENGQINGMUBIAOJIA_QIWANGMUBIAOJIABUNENGWEIKONG : '新申请目标价，期望目标价不能为空'
+}
