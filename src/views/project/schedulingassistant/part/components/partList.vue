62b92812c8b00a9ae91210fde064f3e6f0b29e66
leobai <leobai@deloitte.com.cn>
2022-03-15 17:03:50 +0800

    处理月份问题

--- src/views/project/schedulingassistant/part/components/partList.vue
+++ src/views/project/schedulingassistant/part/components/partList.vue
@@ -406,6 +406,9 @@ export default {
         if(+day <10){
           day = '0'+day
         }
+        if(+month <10){
+          month = '0'+month
+        }
         return year + '-' + month + '-' + day
       }
     }, 
