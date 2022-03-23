2ad2c1949cf10129ef6c2e5233268f7ff5bbb5da
wentliao <wentliao@deloitte.com.cn>
2022-03-23 16:03:39 +0800

    fix bug CRW-7341

--- public/locale/lang/zh.js
+++ public/locale/lang/zh.js
@@ -1,4 +1,4 @@
-;(function (global, factory) {
+(function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined'
 		? (module.exports = factory(require('i18n')))
 		: typeof define === 'function' && define.amd
@@ -6099,7 +6099,7 @@
 		定点类型: '定点类型',
 		XUANZHONGDELINGJIANZHONGBUNENGHANYOUWEICANYUDELINGJIAN:
 			'选中的零件中不能含有未参与的零件',
-		LK_DIFENGXIAN: '低风险',
+		LK_DIFENGXIAN: '中风险',
 		LK_WUZHENGCHANG: '无风险',
 		LK_GAOFENGXIAN: '高风险',
 		BIDDING_SKGYSBKXJ: '受控供应商不可询价！',
