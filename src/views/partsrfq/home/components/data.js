b6bc07dc4a84fa17e287764c6a507056d7487b41
余继鹏 <917955345@qq.com>
2022-03-23 16:47:03 +0800

    feature_CRW-6419

--- src/views/partsrfq/home/components/data.js
+++ src/views/partsrfq/home/components/data.js
@@ -1,24 +1,24 @@
 /*
  * @Author: moxuan
  * @Date: 2021-03-03 09:52:00
- * @LastEditTime: 2021-12-30 17:14:30
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-03-23 16:40:53
+ * @LastEditors: YoHo
  * @Description: In User Settings Edit
  * @FilePath: \front-web\src\views\partsrfq\home\components\data.js
  */
 import { permissionTitle } from '@/utils'
 export const tableTitle = [
 // export const tableTitle = permissionTitle("PARTSRFQ_RFQLIST", [
-    { props: 'id', name: 'RFQ编号', key: 'RFQBIANHAO', tooltip: true, width:80 },
+    { props: 'id', name: 'RFQ编号', key: 'RFQBIANHAO', tooltip: true, width:100 },
     { props: 'rfqName', name: 'RFQ名称', key: 'RFQMINGCHENG', tooltip: true, width: 280 },
     { props: 'rfqType', name: 'RFQ类型', key: 'RFQLEIXING', tooltip: true },
     // { props: 'carType', name: '车型项目', key: 'CHEXINGXIANGMU', tooltip: true, width: 280 },
-    { props: 'createDate', name: '创建日期', key: 'CHUANGJIANRIQI', tooltip: true,width:100 },
+    { props: 'createDate', name: '创建日期', key: 'CHUANGJIANRIQI', tooltip: true,width:120 },
     { props: 'currentRounds', name: '当前轮次', key: 'DANGQIANLUNCI', tooltip: true },
     { props: 'suppliers', name: '已报价/已询价供应商', key: 'YIBAOJIA_YIXUNJIA_GONGYINGSHANG', tooltip: true,width:140 },
     // { props: 'quotations', name: '已报价供应商', key: 'YIBAOJIAGONGYINGSHANG', tooltip: true,width:100 },
     { props: 'currentStatus', name: 'RFQ状态', key: 'RFQZHUANGTAI', tooltip: true },
-    { props: 'currentRoundsEndTime', name: '报价截至日期', key: 'BAOJIAJIEZHIRIQI', tooltip: true,width:100},
+    { props: 'currentRoundsEndTime', name: '报价截至日期', key: 'BAOJIAJIEZHIRIQI', tooltip: true,width:120},
     { props: 'buyerName', name: '询价采购员', key: 'XUNJIACAIGOUYUAN', tooltip: true },
     { props: 'linieNameZh', name: 'LINIE', key: 'LINIE', tooltip: true,width:100 },
     { props: 'tpGradeStatus', name: '技术评分状态', key: 'JISHUPINGFENZHUANGTAI', tooltip: true, width: 100 },
