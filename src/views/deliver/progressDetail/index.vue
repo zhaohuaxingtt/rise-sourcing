397dc05b7e6057510315e4c1d3c2fb23d45655b8
EXT15805 <1198593817@qq.com>
2022-09-28 20:26:54 +0800

    送样管理

--- src/views/deliver/progressDetail/index.vue
+++ src/views/deliver/progressDetail/index.vue
@@ -9,7 +9,7 @@
           <span class="form-title lineH40">车型项目：</span>
           <el-form inline class="form">
             <el-form-item>
-                <iSelect filterable v-model="searchParams.carProject" :placeholder="language('QINGXUANZE','请选择')">
+                <iSelect filterable v-model="searchParams.carTypeProId" :placeholder="language('QINGXUANZE','请选择')">
                   <el-option
                     v-for="item in carProjectOptions"
                     :key="item.cartypeProId"
@@ -21,7 +21,7 @@
           </el-form>
         </div>
         <div class="isearch-top">
-          <iprogress v-if="searchParams.carProject" :carProjectId="searchParams.carProject"></iprogress>
+          <iprogress v-if="searchParams.carTypeProId" :carProjectId="searchParams.carTypeProId"></iprogress>
         </div>
       </div>
       <el-divider></el-divider>
@@ -30,7 +30,7 @@
         <div class="flex-top flex1">
           <el-form class="margin-top10">
             <el-form-item :label="language('CAILIAOZU', '材料组')">
-              <iSelect filterable v-model="searchParams.dept"  :placeholder="language('QINGXUANZE','请选择')">
+              <iSelect filterable v-model="searchParams.materialGroupId"  :placeholder="language('QINGXUANZE','请选择')">
                 <el-option
                   v-for="item in deptList"
                   :key="item.materialGroupId"
@@ -41,7 +41,7 @@
             </el-form-item>
 
             <el-form-item :label="language('LINGJIAN', '零件')">
-              <iSelect filterable v-model="searchParams.part" :placeholder="language('QINGXUANZE','请选择')">
+              <iSelect filterable v-model="searchParams.partNum" :placeholder="language('QINGXUANZE','请选择')">
                 <el-option
                   v-for="item in partList"
                   :key="item.partNum"
@@ -52,7 +52,7 @@
             </el-form-item>
 
             <el-form-item :label="language('GONGYINGSHANG', '供应商')">
-              <iSelect filterable v-model="searchParams.supplier" :placeholder="language('QINGXUANZE','请选择')">
+              <iSelect filterable v-model="searchParams.supplierId" :placeholder="language('QINGXUANZE','请选择')">
                 <el-option
                   v-for="item in supplierList"
                   :key="item.id"
@@ -73,10 +73,10 @@
     </iSearch>
     <iTabsList type="card" class='margin-top20' v-model="tabVal" @tab-click="changeTab" >
       <el-tab-pane name="1" label="HeavyItem零件">
-        <heavyItem :carProjectId="searchParams.carProject" v-if="searchParams.carProject" />
+        <heavyItem :carProjectId="searchParams.carTypeProId" v-if="searchParams.carTypeProId" />
       </el-tab-pane>
       <el-tab-pane name="2" label="普通零件">
-        <commonParts :carProjectId="searchParams.carProject" v-if="searchParams.carProject" />
+        <commonParts :carProjectId="searchParams.carTypeProId" v-if="searchParams.carTypeProId" />
       </el-tab-pane>
     </iTabsList>
   </iPage>
@@ -93,6 +93,8 @@ import {
   getCartypeProMaterialGroup,
   getCartypeProPart,
   getCartypeProSupplier,
+  partsPage,
+  getPartNode,
 } from "@/api/project/deliver";
 
 export default {
@@ -102,10 +104,10 @@ export default {
   data() {
     return {
       searchParams: {
-        carProject:"",
-        dept:"",
-        part:"",
-        supplier:"",
+        carTypeProId:"",
+        materialGroupId:"",
+        partNum:"",
+        supplierId:"",
       },
 
       carProjectOptions:[],
@@ -113,23 +115,47 @@ export default {
       partList:[],
       supplierList:[],
 
-
       selectOptions:{},
       tabVal:'1',
+
+      partNum:'',
     }
   },
   created(){
     this.getData();
   },
   methods:{
-    getData(){
+    async getData(){
       cartype_pro_List({}).then(res=>{
         if(res?.result){
           this.carProjectOptions = res.data.filter(res => res)
-          this.searchParams.carProject = this.carProjectOptions[0]?.cartypeProId;
-
-          this.getGroup(this.searchParams.carProject);//获取车型项目材料组下拉
+          this.searchParams.carTypeProId = this.carProjectOptions[0]?.cartypeProId;
+          this.getGroup(this.searchParams.carTypeProId);//获取车型项目材料组下拉
         }
+      })
+      await this.partsPage();
+      this.getPartNode(this.partNum);
+    },
+    partsPage(){
+      return new Promise((resolve,reject) => {
+        partsPage({
+          ...this.searchParams,
+          current:3,
+          size:1,
+        }).then(res=>{
+          if(res?.result){
+            this.partNum = res.data[0].partsNum
+            resolve();
+          }
+        })
+      })
+    },
+    getPartNode(){
+      console.log(this.partNum)
+      getPartNode({
+        partNum:this.partNum,
+      }).then(res=>{
+        console.log(res);
       })
     },
     getGroup(val){
