6fc5874b1f9914406575ca997861d37d0b2176d6
余继鹏 <917955345@qq.com>
2022-12-07 15:29:34 +0800

    M签字单页面调整

--- src/views/designate/home/signSheet/chipDetails/index.vue
+++ src/views/designate/home/signSheet/chipDetails/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: youyuan
  * @Date: 2021-11-06 17:50:24
- * @LastEditTime: 2022-11-29 22:59:23
+ * @LastEditTime: 2022-12-07 15:16:10
  * @LastEditors: 余继鹏 917955345@qq.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \front-web\src\views\designate\home\signSheet\components\mtzDetails\index.vue
@@ -140,6 +140,7 @@ export default {
         val.forEach((o) => {
           // 做前端标记
           o.flagSelect = true;
+          o.ttNominateAppId = o.appNo
         });
       this.$set(this, "tableListData", this.tableListData.concat(val));
     },
