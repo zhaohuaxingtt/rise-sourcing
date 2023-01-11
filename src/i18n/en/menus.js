5af6a5ff8b31b09d255a5f802bdb158f697d1e8e
余继鹏 <917955345@qq.com>
2023-01-11 13:16:38 +0800

    菜单国际化

--- src/i18n/en/menus.js
+++ src/i18n/en/menus.js
@@ -7,6 +7,8 @@ export default {
 	审批中心: 'Approval Center',
 	下载中心: 'Download Center',
 	项目管理: 'Project Management',
+	送样管理: 'Sample Management',
+	产能维护及报警信: 'Capacity & Alarm',
 	模具投资: 'Tooling Investment',
 	寻源与定点: 'Sourcing & Nomination',
 	财务管理: 'Finance Management',
@@ -58,5 +60,5 @@ export default {
 	'新闻&公告模块管理': 'News & Announcements Module Mgt',
 	设置: 'Setting',
 	退出登录: 'Logout',
-	确认:'Sure'
+	确认: 'Sure'
 }
