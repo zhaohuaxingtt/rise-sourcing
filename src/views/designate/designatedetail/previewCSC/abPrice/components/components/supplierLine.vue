b6ff469e5003e48b4e6c13c94928b6441620f6eb
余继鹏 <917955345@qq.com>
2023-02-12 21:54:03 +0800

    update

--- src/views/designate/designatedetail/previewCSC/abPrice/components/components/supplierLine.vue
+++ src/views/designate/designatedetail/previewCSC/abPrice/components/components/supplierLine.vue
@@ -2,7 +2,7 @@
  * @Author: 余继鹏 917955345@qq.com
  * @Date: 2023-02-02 23:24:33
  * @LastEditors: 余继鹏 917955345@qq.com
- * @LastEditTime: 2023-02-12 21:19:45
+ * @LastEditTime: 2023-02-12 21:53:55
  * @FilePath: \front-web\src\views\designate\designatedetail\previewCSC\abPrice\components\components\supplierBar.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -20,7 +20,7 @@
       :show-header="false"
       :span-method="arraySpanMethod"
     >
-      <el-table-column prop="supplierName" align="left" :width="labelWidth">
+      <el-table-column prop="supplierNameEn" align="left" :width="labelWidth">
         <template slot-scope="scope">
           <div v-if="scope.$index == 0" class="chart" ref="chart"></div>
         </template>
@@ -38,7 +38,7 @@
         ref="table"
         :show-header="false"
       >
-        <el-table-column prop="supplierName" align="left" :width="labelWidth">
+        <el-table-column prop="supplierNameEn" align="left" :width="labelWidth">
           <template slot-scope="scope">
             <div class="flex">
               <div class="legend margin-right5">
@@ -52,7 +52,7 @@
                 ></span>
               </div>
               <span>
-                {{ scope.row.supplierName }}
+                {{ scope.row.supplierNameEn }}
               </span>
             </div>
           </template>
@@ -189,7 +189,7 @@ export default {
       let series = [];
       this.tableData.forEach((item, index) => {
         let seriesItem = {
-          name: item.supplierName,
+          name: item.supplierNameEn,
           type: "line",
           label: {
             show: true,
