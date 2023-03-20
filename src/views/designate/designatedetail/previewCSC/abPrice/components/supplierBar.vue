fedc51b5df561690d2654c4eed1af0eef603d2e5
余继鹏 <917955345@qq.com>
2023-03-20 18:32:32 +0800

    update 隐藏弹窗

--- src/views/designate/designatedetail/previewCSC/abPrice/components/supplierBar.vue
+++ src/views/designate/designatedetail/previewCSC/abPrice/components/supplierBar.vue
@@ -2,8 +2,8 @@
  * @Author: 余继鹏 917955345@qq.com
  * @Date: 2023-02-02 23:24:33
  * @LastEditors: 余继鹏 917955345@qq.com
- * @LastEditTime: 2023-03-13 11:31:10
- * @FilePath: \front-web\src\views\designate\designatedetail\previewCSC\abPrice\components\components\supplierBar.vue
+ * @LastEditTime: 2023-03-20 18:30:03
+ * @FilePath: \front-web\src\views\designate\designatedetail\previewCSC\abPrice\components\supplierBar.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
@@ -228,7 +228,7 @@
                 :data="item"
                 :max="max"
               />
-              <template v-else-if="item.prop == 'Recommendation'">
+              <!-- <template v-else-if="item.prop == 'Recommendation'">
               <el-popover
                 placement="right"
                 trigger="hover"
@@ -248,7 +248,7 @@
                   />
                 </div>
               </el-popover>
-              </template>
+              </template> -->
               <barItem
                 v-else
                 :key="item.prop"
