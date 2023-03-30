c2ffae78f7bab369bd7c713760a994df3141a69e
余继鹏 <917955345@qq.com>
2023-03-30 18:09:19 +0800

    CRW-12601

--- src/views/designate/designatedetail/previewCSC/abPrice/components/barItemKGF.vue
+++ src/views/designate/designatedetail/previewCSC/abPrice/components/barItemKGF.vue
@@ -21,15 +21,17 @@ export default {
   watch:{
     height(){
       this.resize()
+    },
+    data:{
+      handler(){
+        this.drawBar();
+      },
+      deep:true
     }
   },
   mounted() {
     window.addEventListener("resize", this.resize);
     this.drawBar();
-    this.resize();
-  },
-  updated() {
-    this.resize();
   },
   methods: {
     getDiff() {
@@ -173,7 +175,7 @@ export default {
     resize() {
       this.$nextTick(() => {
         setTimeout(() => {
-          this.charts.resize();
+          this.charts&&this.charts.resize();
         }, 32);
       });
     },
