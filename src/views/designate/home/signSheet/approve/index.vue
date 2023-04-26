df9a85599dd40c8c5ddc4abf1b0e44ecce0462d8
余继鹏 <917955345@qq.com>
2023-04-26 15:00:55 +0800

    暂存

--- src/views/designate/home/signSheet/approve/index.vue
+++ src/views/designate/home/signSheet/approve/index.vue
@@ -1,15 +1,47 @@
 <template>
-    <div>
-        审批页面
-    </div>
+  <iTabsList type="card" v-model="tab">
+    <el-tab-pane
+      v-for="(tab, $index) in tabs"
+      :key="$index"
+      :label="tab.name"
+      :name="tab.key"
+    >
+      <div class="margin-top20">
+        <component :ref="tab.key" :is="tab.component" />
+      </div>
+    </el-tab-pane>
+  </iTabsList>
 </template>
 
 <script>
-    export default {
-        
-    }
+import { iTabsList } from "rise";
+import approved from "./approved";
+import pending from "./pending";
+export default {
+  components: {
+    iTabsList,
+    approved,
+    pending,
+  },
+  data() {
+    return {
+      tab: "pending",
+      tabs: [
+        {
+          label: "pending",
+          component: "pending",
+          key: "pending",
+        },
+        {
+          label: "approved",
+          component: "approved",
+          key: "approved",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
